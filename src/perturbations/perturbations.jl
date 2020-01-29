daa442e3e8fe74bb1df0b1d99eceac436a9c4774
Jose Daniel Lara <jdlara@berkeley.edu>
2020-01-29 15:17:34 -0800

    add more fields to perturbations

--- src/perturbations/perturbations.jl
+++ src/perturbations/perturbations.jl
@@ -2,10 +2,12 @@ abstract type Perturbation end
 
 struct ThreePhaseFault
     time::Float64
-    Ybus::SparseMatrixCSC{Complex{Float64}, Int64}
+    Ybus #::SparseMatrixCSC{Complex{Float64}, Int64}
 end
 
 struct ControlStepUp
+    time::Float64
     device::PSY.DynamicInjection
     signal::Symbol
+    additional_value::Float64
 end
