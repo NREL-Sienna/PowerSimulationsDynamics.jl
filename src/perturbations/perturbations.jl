966445c090cfebffb764e07549aa7690b1720c4b
Jose Daniel Lara <jdlara@berkeley.edu>
2020-01-29 17:59:23 -0800

    update perturbations

--- src/perturbations/perturbations.jl
+++ src/perturbations/perturbations.jl
@@ -1,13 +1,21 @@
 abstract type Perturbation end
 
-struct ThreePhaseFault
+struct ThreePhaseFault <: Perturbation
     time::Float64
     Ybus #::SparseMatrixCSC{Complex{Float64}, Int64}
 end
 
-struct ControlStepUp
+get_affect(pert::ThreePhaseFault) = (integrator) -> PSY.get_ext(integrator.p)["Ybus"] = pert.Ybus
+
+struct ControlReferenceChange <: Perturbation
     time::Float64
     device::PSY.DynamicInjection
-    signal::Symbol
-    additional_value::Float64
+    signal_index::Int64
+    ref_value::Float64
+end
+
+function get_affect(pert::ControlReferenceChange)
+    return (integrator) -> begin
+        return PSY.get_ext(pert.device)[CONTROL_REFS][pert.signal_index] = pert.ref_value
+    end
 end
