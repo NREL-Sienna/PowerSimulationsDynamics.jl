a688ee21ac509ca5ed721849ca3cf45924c1db2d
Jose Daniel Lara <jdlara@berkeley.edu>
2020-01-30 18:20:46 -0800

    changes to support forward diff

--- src/models/ref_transformations.jl
+++ src/models/ref_transformations.jl
@@ -6,9 +6,25 @@ end
     R = 1
     I = 2
 end
+function dq_ri(δ::Real)
+    ## Uses the referenceframe of the Kundur page 852 of dq to RI
+    return [
+        sin(δ) cos(δ)
+        -cos(δ) sin(δ)
+    ]
+end
+
+function ri_dq(δ::Real)
+    #Uses the reference frame of the Kundur page 852 of RI to dq
+    return [
+        sin(δ) -cos(δ)
+        cos(δ) sin(δ)
+    ]
+end
+
 function dq_ri(δ::Float64)
     ## Uses the referenceframe of the Kundur page 852 of dq to RI
-    dq_ri = [
+    return [
         sin(δ) cos(δ)
         -cos(δ) sin(δ)
     ]
@@ -16,7 +32,7 @@ end
 
 function ri_dq(δ::Float64)
     #Uses the reference frame of the Kundur page 852 of RI to dq
-    ri_dq = [
+    return [
         sin(δ) -cos(δ)
         cos(δ) sin(δ)
     ]
