ba7058e094bb048cbb1db24fa17503f083cb2ee7
Jose Daniel Lara <jdlara@berkeley.edu>
2020-03-19 22:08:55 -0600

    don't pass dx to line

--- src/models/kirchoff_laws.jl
+++ src/models/kirchoff_laws.jl
@@ -7,17 +7,31 @@
 
 """
 #TODO: Improve performance of this function
-function kirchoff_laws(sys, V_r, V_i, I_injections_r, I_injections_i)
+function kirchoff_laws(sys, V_r, V_i, I_injections_r, I_injections_i, dx)
     Ybus = PSY.get_ext(sys)[YBUS]
     I_bus = Ybus * (V_r + V_i .* 1im)
     I_balance = [real(I_bus) - I_injections_r; imag(I_bus) - I_injections_i]
+
+    voltage_buses = PSY.get_ext(sys)["voltage_buses_ix"]
+    isempty(voltage_buses) && return I_balance
+
+    sys_f = PSY.get_frequency(sys)
+    ω_b = 2.0 * π * sys_f
+    n_buses = length(PSY.get_components(PSY.Bus, sys))
+
+    for bus_no in voltage_buses
+        shunt_multiplier = PSY.get_ext(sys)["total_shunts"][bus_no]
+        I_balance[bus_no] = -ω_b*I_balance[bus_no]*shunt_multiplier + ω_b*V_i[bus_no] - dx[bus_no]
+        I_balance[bus_no + n_buses] = -ω_b*I_balance[bus_no+n_buses]*shunt_multiplier - ω_b*V_i[bus_no+n_buses] - dx[bus_no+n_buses]
+    end
+
     return I_balance
 end
-
-
 #=
-function kvl(I_net, B_eq)
-    return [I_net[1].*B_eq ;
-            I_net[2].*B_eq]
-end
-=#
+            ix_dx = [
+                from_bus_number,
+                from_bus_number + bus_size,
+                to_bus_number,
+                to_bus_number + bus_size,
+            ]
+            =#
