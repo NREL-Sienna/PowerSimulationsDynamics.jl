8e52aac590cee35b542e6107cb99dcabe16e274b
Jose Daniel Lara <jdlara@berkeley.edu>
2020-04-27 18:07:47 -0700

    update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,6 @@ using Sundials
 
 const PSY = PowerSystems
 
-include("./data_tests/network_test_data.jl")
-include("./data_tests/dynamic_test_data.jl")
-
 tests = readdir(dirname(@__FILE__))
 tests = filter(
     f -> startswith(f, "test_") && endswith(f, ".jl") && f != basename(@__FILE__),
@@ -17,11 +14,9 @@ tests = filter(
 )
 
 @testset "BasicTests" begin
-
     for test in tests
         print(splitext(test)[1], ": ")
         include(test)
         println()
     end
-
 end
