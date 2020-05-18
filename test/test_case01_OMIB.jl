625bf35d1425b3a5ee5096b528c93817550b9e22
Rodrigo Henriquez <rodrigomha@gmail.com>
2020-05-17 19:56:12 -0700

    Update tolerance

--- test/test_case01_OMIB.jl
+++ test/test_case01_OMIB.jl
@@ -37,6 +37,6 @@ series = get_state_series(sim, ("generator-2-1", :δ));
 series2 = get_voltagemag_series(sim, 2)
 LITS.print_init_states(sim)
 
-@test LinearAlgebra.norm(sim.x0_init - test01_x0_init) < 1e-5
+@test LinearAlgebra.norm(sim.x0_init - test01_x0_init) < 1e-6
 @test sim.solution.retcode == :Success
 @test small_sig.stable
