d1901fd7a014aac034ac9d04da8ff50e0db4130e
Rodrigo Henriquez <rodrigomha@gmail.com>
2020-07-06 22:39:57 -0700

    update test 5

--- test/data_tests/test05.jl
+++ test/data_tests/test05.jl
@@ -13,7 +13,7 @@ add_source_to_ref(threebus_sys)
 for g in get_components(Generator, threebus_sys)
     g.activepower = 0.75
 end
-res = solve_powerflow!(threebus_sys, nlsolve)
+res = solve_powerflow!(threebus_sys)
 
 function dyn_gen_five_mass_shaft_order(generator)
     return PSY.DynamicGenerator(
--- test/test_case05_5shaft.jl
+++ test/test_case05_5shaft.jl
@@ -17,41 +17,6 @@ include(joinpath(dirname(@__FILE__), "data_tests/test05.jl"))
 
 tspan = (0.0, 20.0);
 
-#Initial guess
-#Initial guess
-x0_guess = [
-    1.02,
-    1.0,
-    1.0,
-    0.0,
-    -0.01,
-    -0.01,
-    1.0, #eq_p
-    0.0, #ed_p
-    0.05, #δ
-    1.0, #ω
-    0.05, #δ_HP
-    1.0, #ω_HP
-    0.05, #δ_IP
-    1.0, #ω_LP
-    0.05, #δ_LP
-    1.0, #ω_LP
-    0.05, #δ_Ex
-    1.0, #ω_Ex
-    1.0, #Vf
-    0.01, #Vr1
-    -0.1, #Vr2,
-    1.0, #Vm
-    0.81, #eq_p
-    0.59, #ed_p
-    0.6, #δ
-    1.0, #ω
-    1.7, #Vf
-    0.11, #Vr1
-    -0.31, #Vr2,
-    1.0, #Vm
-]
-
 #Define Fault: Change of YBus
 Ybus_change = ThreePhaseFault(
     1.0, #change at t = 1.0
@@ -63,7 +28,6 @@ sim = Simulation(
     threebus_sys, #system
     tspan, #time span
     Ybus_change, #Type of Fault
-    initial_guess = x0_guess,
 ) #initial guess
 
 #Solve problem in equilibrium
@@ -72,10 +36,10 @@ run_simulation!(sim, IDA(), dtmax = 0.001);
 small_sig = small_signal_analysis(sim)
 
 #Obtain data for angles
-series = get_state_series(sim, ("generator-3-2", :δ));
-series2 = get_state_series(sim, ("generator-3-2", :δ_hp));
-series3 = get_state_series(sim, ("generator-3-2", :δ_ip));
-series4 = get_state_series(sim, ("generator-3-2", :δ_ex));
+series = get_state_series(sim, ("generator-3-1", :δ));
+series2 = get_state_series(sim, ("generator-3-1", :δ_hp));
+series3 = get_state_series(sim, ("generator-3-1", :δ_ip));
+series4 = get_state_series(sim, ("generator-3-1", :δ_ex));
 
 @test LinearAlgebra.norm(sim.x0_init - test05_x0_init) < 1e-6
 @test sim.solution.retcode == :Success
