4662f4d4675c46296a7126c68e55d18c30d3309f
Rodrigo Henriquez <rodrigomha@gmail.com>
2020-07-20 18:28:43 -0700

    Update Test 14

--- test/test_case14_inverter_ref.jl
+++ test/test_case14_inverter_ref.jl
@@ -1,7 +1,7 @@
 """
-Case 12:
-This case study a three bus system with 2 machines (Classic Model - Single Shaft: 2 State model) without loads.
-The machine at bus 1 is used as a reference machine, while machine at bus 2 has a simplified droop governor (TGTypeII).
+Case 14:
+This case study a three bus system with 1 machines (Classic Model - Single Shaft: 2 State model) and 1 Inverter without loads.
+The inverter at bus 1 is used as a reference device, while machine at bus 2 has a simplified droop governor (TGTypeII).
 The perturbation trips four (out of 5) circuits of line between buses 1 and 2, multiplying by 4 its impedance.
 """
 
@@ -9,14 +9,14 @@ The perturbation trips four (out of 5) circuits of line between buses 1 and 2, m
 ############### LOAD DATA ########################
 ##################################################
 
-include(joinpath(dirname(@__FILE__), "data_tests/test12.jl"))
+include(joinpath(dirname(@__FILE__), "data_tests/test14.jl"))
 
 ##################################################
 ############### SOLVE PROBLEM ####################
 ##################################################
 
 #Time span
-tspan = (0.0, 5.0)
+tspan = (0.0, 10.0)
 
 #Define Fault: Change of YBus
 Ybus_change = ThreePhaseFault(
@@ -24,7 +24,7 @@ Ybus_change = ThreePhaseFault(
     Ybus_fault,
 ) #New YBus
 
-path = (joinpath(pwd(), "test-12"))
+path = (joinpath(pwd(), "test-14"))
 !isdir(path) && mkdir(path)
 try
     sim = Simulation(
@@ -40,14 +40,14 @@ try
     run_simulation!(
         sim, #simulation structure
         IDA(),#Sundials DAE Solver
-        dtmax = 0.02, #keywords arguments
+        dtmax = 0.001, #keywords arguments
     )
 
-    series = get_state_series(sim, ("generator-102-1", :ω))
+    series = get_state_series(sim, ("generator-101-1", :ω_oc))
 
     diff = [0.0]
     res = get_init_values_for_comparison(sim)
-    for (k, v) in test12_x0_init
+    for (k, v) in test14_x0_init
         diff[1] += LinearAlgebra.norm(res[k] - v)
     end
     @test (diff[1] < 1e-3)
