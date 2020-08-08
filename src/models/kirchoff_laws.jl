98e86c5dee93af3c9aef573caeaeb0322dd7146c
Jose Daniel Lara <jdlara@berkeley.edu>
2020-08-08 13:45:49 -0700

    fixes to Kirchoff Law functions

--- src/models/kirchoff_laws.jl
+++ src/models/kirchoff_laws.jl
@@ -1,12 +1,13 @@
 function Ybus_current_kirchoff(
-    Ybus,
+    inputs,
     V_r,
     V_i,
     I_injections_r,
     I_injections_i,
-    I_bus,
-    I_balance,
 )
+    I_bus = get_aux_arrays(inputs)[5]
+    I_balance = get_aux_arrays(inputs)[6]
+    Ybus = get_Ybus(inputs)
     # Note: BLAS doesn't work because the the type of Vr and Vi is not Matrix of Complex
     LinearAlgebra.mul!(I_bus, Ybus, (V_r + V_i .* 1im))
     bus_count = length(I_bus)
@@ -19,13 +20,14 @@ function Ybus_current_kirchoff(
 end
 
 function voltage_kirchoff(inputs::SimulationInputs, V_r, V_i, dx)
+    I_balance = get_aux_arrays(inputs)[6]
     voltage_buses_ix = get_voltage_buses_ix(inputs)
     isempty(voltage_buses_ix) && return
     sys = get_system(inputs)
     sys_f = PSY.get_frequency(sys)
     ω_b = 2.0 * π * sys_f
     n_buses = get_bus_count(inputs)
-    shunts = get_total_shunts(sys)
+    shunts = get_total_shunts(inputs)
     for bus_ix in voltage_buses_ix
         shunt_multiplier = shunts[bus_ix]
         I_balance[bus_ix] =
@@ -51,10 +53,7 @@ function kirchoff_laws!(
     I_injections_i,
     dx,
 )
-    I_bus = get_aux_arrays(inputs)[5]
-    I_balance = get_aux_arrays(inputs)[6]
-    Ybus = get_Ybus(inputs)
-    Ybus_current_kirchoff(Ybus, V_r, V_i, I_injections_r, I_injections_i, I_bus, I_balance)
+    Ybus_current_kirchoff(inputs, V_r, V_i, I_injections_r, I_injections_i)
     voltage_kirchoff(inputs, V_r, V_i, dx)
     return
 end
