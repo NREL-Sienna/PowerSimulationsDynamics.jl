cd19ddb6b58f7545053e961cee0ba1cb1f1ee4bd
Jose Daniel Lara <jdlara@berkeley.edu>
2020-08-09 07:05:30 -0700

    expand dispatch of saturation

--- src/models/saturation_models.jl
+++ src/models/saturation_models.jl
@@ -2,7 +2,7 @@
     Saturation function for quadratic saturation models for machines
         Se(x) = B * (x - A)^2 / x
 """
-function saturation_function(machine::PSY.RoundRotorQuadratic, x)
+function saturation_function(machine::Union{PSY.RoundRotorQuadratic, PSY.SalientPoleQuadratic}, x)
     Sat_A, Sat_B = PSY.get_saturation_coeffs(machine)
     return Sat_B * (x - Sat_A)^2 / x
 end
@@ -11,7 +11,7 @@ end
     Saturation function for exponential saturation models for machines
         Se(x) = B * x^A
 """
-function saturation_function(machine::PSY.RoundRotorExponential, x)
+function saturation_function(machine::Union{PSY.RoundRotorExponential, PSY.SalientPoleExponential}, x)
     Sat_A, Sat_B = PSY.get_saturation_coeffs(machine)
     return Sat_B * x^Sat_A
 end
