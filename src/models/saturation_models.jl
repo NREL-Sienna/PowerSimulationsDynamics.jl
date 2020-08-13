b9d864598a94943359b437e469e43f9c4aa3148d
Rodrigo Henriquez <rodrigomha@gmail.com>
2020-08-12 23:55:53 -0700

    WIP: update AC1A model

--- src/models/saturation_models.jl
+++ src/models/saturation_models.jl
@@ -21,3 +21,17 @@ function saturation_function(
     Sat_A, Sat_B = PSY.get_saturation_coeffs(machine)
     return Sat_B * x^Sat_A
 end
+
+function rectifier_function(I::Float64)
+    if I < 0.0
+        return 1.0
+    elseif I <= 0.433
+        return 1 - 0.577 * I
+    elseif I < 0.75
+        return sqrt(0.75 - I^2)
+    elseif I <= 1.0
+        return 1.732 * (1 - I)
+    else
+        return 0.0
+    end
+end
