626a224cf107e1f61e63762ec3fa70e434d3353b
Rodrigo Henriquez <rodrigomha@gmail.com>
2021-05-18 16:34:31 -0700

    fix type inference: Co-authored-by: Jose Daniel Lara <jdlara@berkeley.edu>

---
+++ src/post_processing/pp_generator.jl
@@ -0,0 +1,76 @@
+"""
+Function to obtain the output current time series of a Base Machine model out of the DAE Solution. It receives the simulation inputs and
+the dynamic device. It is dispatched for device type to compute the specific current.
+
+"""
+function compute_output_current(
+    sim::Simulation,
+    dynamic_device::G,
+    V_R::Vector{Float64}, 
+    V_I::Vector{Float64}
+) where {G<:PSY.DynamicGenerator}
+
+    #Obtain Data
+    sys = get_system(sim.simulation_inputs)
+    
+    #Get machine
+    machine = PSY.get_machine(dynamic_device)
+    Sbase = PSY.get_base_power(sys)
+    basepower = PSY.get_base_power(dynamic_device)
+    base_power_ratio = basepower / Sbase
+    return _machine_current(machine, PSY.get_name(dynamic_device), V_R, V_I, base_power_ratio, sim)
+end
+
+function _machine_current(machine::PSY.BaseMachine, name::String, V_R::Vector{Float64}, V_I::Vector{Float64}, base_power_ratio::Float64, sim::Simulation)
+    δ = pp_state_series(sim, (name, :δ))
+
+    R = PSY.get_R(machine)
+    Xd_p = PSY.get_Xd_p(machine)
+    eq_p = PSY.get_eq_p(machine)
+    
+    i_dq = Vector{Float64}(undef, 2)
+    I_R = similar(δ)
+    I_I = similar(δ)
+        
+    for ix in 1:length(δ)
+        v = δ[ix]
+        V_d, V_q = ri_dq(v) * [V_R[ix]; V_I[ix]]
+        #Obtain electric current
+        i_dq[1] = (1.0 / (R^2 + Xd_p^2)) * (Xd_p * (eq_p .- V_q) - R * V_d)  #15.36
+        i_dq[2] = (1.0 / (R^2 + Xd_p^2)) * (Xd_p * V_d + R * (eq_p .- V_q)) #15.36
+
+        I_R[ix], I_I[ix] = base_power_ratio * dq_ri(v) * i_dq
+    end
+    return I_R, I_I
+end
+
+
+"""
+Function to obtain the output current time series of a One-D-One-Q model out of the DAE Solution. It receives the simulation inputs and
+the dynamic device. It is dispatched for device type to compute the specific current.
+
+"""
+function _machine_current(machine::PSY.OneDOneQMachine, name::String, V_R::Vector{Float64}, V_I::Vector{Float64}, base_power_ratio::Float64, sim::Simulation)
+    δ = pp_state_series(sim, (name, :δ))
+    eq_p = pp_state_series(sim, (name, :eq_p))
+    ed_p = pp_state_series(sim, (name, :ed_p))
+
+    R = PSY.get_R(machine)
+    Xd_p = PSY.get_Xd_p(machine)
+    Xq_p = PSY.get_Xq_p(machine)
+
+    i_dq = Vector{Float64}(undef, 2)
+    I_R = similar(δ, Float64)
+    I_I = similar(δ, Float64)
+
+    for ix in 1:length(δ)
+        v = δ[ix]
+        V_d, V_q = ri_dq(v) * [V_R[ix]; V_I[ix]]
+        #Obtain electric current
+        i_dq[1] = (1.0 / (R^2 + Xd_p * Xq_p)) * (Xq_p * (eq_p[ix] - V_q) + R * (ed_p[ix] - V_d))  #15.32
+        i_dq[2] = (1.0 / (R^2 + Xd_p * Xq_p)) * (-Xd_p * (ed_p[ix] - V_d) + R * (eq_p[ix] - V_q))  #15.32
+
+        I_R[ix], I_I[ix] = base_power_ratio * dq_ri(v) * i_dq
+    end
+    return I_R, I_I
+end
