c52d4c0d78bfedef35593513b5fb4c876a50aab4
Jose Daniel Lara <jdlara@berkeley.edu>
2021-06-01 17:57:42 -0700

    add method calls to device

--- src/models/device.jl
+++ src/models/device.jl
@@ -1,6 +1,22 @@
+function device_mass_matrix_entries!(mass_matrix, dynamic_device::T) where {T}
+    error("Mass Matrix not implemented for models $T")
+end
+
+function device_mass_matrix_entries!(
+    mass_matrix,
+    dynamic_device::DynG,
+) where {DynG <: PSY.DynamicGenerator}
+    mass_matrix_tg_entries!(mass_matrix, dynamic_device)
+    mass_matrix_pss_entries!(mass_matrix, dynamic_device)
+    mass_matrix_avr_entries!(mass_matrix, dynamic_device)
+    mass_matrix_machine_entries!(mass_matrix, dynamic_device)
+    mass_matrix_shaft_entries!(mass_matrix, dynamic_device)
+    return
+end
+
 function device!(
     x,
-    output_ode,
+    output_ode::Vector{T},
     voltage_r,
     voltage_i,
     current_r,
@@ -9,7 +25,7 @@ function device!(
     ode_range::UnitRange{Int},
     dynamic_device::DynG,
     inputs::SimulationInputs,
-) where {DynG <: PSY.DynamicGenerator}
+) where {DynG <: PSY.DynamicGenerator, T <: Real}
     #Obtain local device states
     device_states = @view x[ix_range]
 
@@ -75,6 +91,19 @@ function device!(
     ::SimulationInputs,
 )
     mdl_Zload!(voltage_r, voltage_i, current_r, current_i, device)
+    return
+end
+
+function device_mass_matrix_entries!(
+    mass_matrix,
+    dynamic_device::DynI,
+) where {DynI <: PSY.DynamicInverter}
+    mass_matrix_DCside_entries!(mass_matrix, dynamic_device)
+    mass_matrix_freq_estimator_entries!(mass_matrix, dynamic_device)
+    mass_matrix_outer_entries!(mass_matrix, dynamic_device)
+    mass_matrix_inner_entries!(mass_matrix, dynamic_device)
+    mass_matrix_converter_entries!(mass_matrix, dynamic_device)
+    mass_matrix_filter_entries!(mass_matrix, dynamic_device)
     return
 end
 
