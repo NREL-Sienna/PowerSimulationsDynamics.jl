6ac5ff5b9cb1e93d9874382f753ff9b6f5d0ace9
Jose Daniel Lara <jdlara@berkeley.edu>
2021-06-01 18:00:11 -0700

    add OMIB test

--- test/test_case01_OMIB.jl
+++ test/test_case01_OMIB.jl
@@ -19,62 +19,123 @@ Ybus_change = NetworkSwitch(
     Ybus_fault,
 ) #New YBus
 
-path = (joinpath(pwd(), "test-01"))
-!isdir(path) && mkdir(path)
-try
-    #Define Simulation Problem
-    sim = Simulation!(
-        ImplicitModel,
-        omib_sys, #system
-        path,
-        (0.0, 20.0), #time span
-        Ybus_change,
-    ) #Type of Fault
+@testset "Test 01 OMIB ImplicitModel" begin
+    path = (joinpath(pwd(), "test-01"))
+    !isdir(path) && mkdir(path)
+    try
+        #Define Simulation Problem
+        sim = Simulation!(
+            ImplicitModel,
+            omib_sys, #system
+            path,
+            (0.0, 20.0), #time span
+            Ybus_change,
+        ) #Type of Fault
 
-    #Obtain small signal results for initial conditions
-    small_sig = small_signal_analysis(sim)
+        #Obtain small signal results for initial conditions
+        small_sig = small_signal_analysis(sim)
 
-    #Solve problem in equilibrium
-    execute!(sim, IDA(), dtmax = 0.005, saveat = 0.005)
+        #Solve problem
+        execute!(sim, IDA(), dtmax = 0.005, saveat = 0.005)
 
-    #Obtain data for angles
-    series = get_state_series(sim, ("generator-102-1", :δ))
-    t = series[1]
-    δ = series[2]
-    #Clean Extra Point at t = 1.0 from Callback
+        #Obtain data for angles
+        series = get_state_series(sim, ("generator-102-1", :δ))
+        t = series[1]
+        δ = series[2]
+        #Clean Extra Point at t = 1.0 from Callback
 
-    series2 = get_voltagemag_series(sim, 102)
+        series2 = get_voltagemag_series(sim, 102)
 
-    #Obtain PSAT benchmark data
-    psat_csv = joinpath(dirname(@__FILE__), "benchmarks/psat/Test01/Test01_delta.csv")
-    psse_csv = joinpath(dirname(@__FILE__), "benchmarks/psse/Test01/Test01_delta.csv")
-    t_psat, δ_psat = get_csv_delta(psat_csv)
-    t_psse, δ_psse = get_csv_delta(psse_csv)
+        #Obtain PSAT benchmark data
+        psat_csv = joinpath(dirname(@__FILE__), "benchmarks/psat/Test01/Test01_delta.csv")
+        psse_csv = joinpath(dirname(@__FILE__), "benchmarks/psse/Test01/Test01_delta.csv")
+        t_psat, δ_psat = get_csv_delta(psat_csv)
+        t_psse, δ_psse = get_csv_delta(psse_csv)
 
-    diff = [0.0]
-    res = get_init_values_for_comparison(sim)
-    for (k, v) in test01_x0_init
-        diff[1] += LinearAlgebra.norm(res[k] - v)
+        diff = [0.0]
+        res = get_init_values_for_comparison(sim)
+        for (k, v) in test01_x0_init
+            diff[1] += LinearAlgebra.norm(res[k] - v)
+        end
+        #Test Initial Condition
+        @test (diff[1] < 1e-3)
+        #Test Solution DiffEq
+        @test sim.solution.retcode == :Success
+        #Test Small Signal
+        @test small_sig.stable
+        #Test Transient Simulation Results
+        @test LinearAlgebra.norm(t - round.(t_psse, digits = 3)) == 0.0
+        # PSSE results are in Degrees
+        @test LinearAlgebra.norm(δ - (δ_psse .* pi / 180), Inf) <= 2e-3
+        @test LinearAlgebra.norm(t - t_psat) == 0.0
+        @test LinearAlgebra.norm(δ - δ_psat, Inf) <= 1e-3
+
+        power = PSID.get_activepower_series(sim, "generator-102-1")
+        rpower = PSID.get_reactivepower_series(sim, "generator-102-1")
+        @test isa(power, Tuple{Vector{Float64}, Vector{Float64}})
+        @test isa(rpower, Tuple{Vector{Float64}, Vector{Float64}})
+
+    finally
+        @info("removing test files")
+        rm(path, force = true, recursive = true)
     end
-    #Test Initial Condition
-    @test (diff[1] < 1e-3)
-    #Test Solution DiffEq
-    @test sim.solution.retcode == :Success
-    #Test Small Signal
-    @test small_sig.stable
-    #Test Transient Simulation Results
-    @test LinearAlgebra.norm(t - round.(t_psse, digits = 3)) == 0.0
-    # PSSE results are in Degrees
-    @test LinearAlgebra.norm(δ - (δ_psse .* pi / 180), Inf) <= 2e-3
-    @test LinearAlgebra.norm(t - t_psat) == 0.0
-    @test LinearAlgebra.norm(δ - δ_psat, Inf) <= 1e-3
+end
 
-    power = PSID.get_activepower_series(sim, "generator-102-1")
-    rpower = PSID.get_reactivepower_series(sim, "generator-102-1")
-    @test isa(power, Tuple{Vector{Float64}, Vector{Float64}})
-    @test isa(rpower, Tuple{Vector{Float64}, Vector{Float64}})
+@testset "Test 01 OMIB MassMatrixModel" begin
+    path = (joinpath(pwd(), "test-01"))
+    !isdir(path) && mkdir(path)
+    try
+        #Define Simulation Problem
+        sim = Simulation!(
+            MassMatrixModel,
+            omib_sys, #system
+            path,
+            (0.0, 20.0), #time span
+            Ybus_change,
+        ) #Type of Fault
 
-finally
-    @info("removing test files")
-    rm(path, force = true, recursive = true)
+        # small_sig = small_signal_analysis(sim)
+
+        execute!(sim, Rodas5(), dtmax = 0.005, saveat = 0.005)
+
+        #Obtain data for angles
+        series = get_state_series(sim, ("generator-102-1", :δ))
+        t = series[1]
+        δ = series[2]
+
+        series2 = get_voltagemag_series(sim, 102)
+
+        #Obtain PSAT benchmark data
+        psat_csv = joinpath(dirname(@__FILE__), "benchmarks/psat/Test01/Test01_delta.csv")
+        psse_csv = joinpath(dirname(@__FILE__), "benchmarks/psse/Test01/Test01_delta.csv")
+        t_psat, δ_psat = get_csv_delta(psat_csv)
+        t_psse, δ_psse = get_csv_delta(psse_csv)
+
+        diff = [0.0]
+        res = get_init_values_for_comparison(sim)
+        for (k, v) in test01_x0_init
+            diff[1] += LinearAlgebra.norm(res[k] - v)
+        end
+        #Test Initial Condition
+        @test (diff[1] < 1e-3)
+        #Test Solution DiffEq
+        @test sim.solution.retcode == :Success
+        #Test Small Signal
+        # @test small_sig.stable
+        #Test Transient Simulation Results
+        @test LinearAlgebra.norm(t - round.(t_psse, digits = 3)) == 0.0
+        # PSSE results are in Degrees
+        @test LinearAlgebra.norm(δ - (δ_psse .* pi / 180), Inf) <= 2e-3
+        @test LinearAlgebra.norm(t - t_psat) == 0.0
+        @test LinearAlgebra.norm(δ - δ_psat, Inf) <= 1e-3
+
+        power = PSID.get_activepower_series(sim, "generator-102-1")
+        rpower = PSID.get_reactivepower_series(sim, "generator-102-1")
+        @test isa(power, Tuple{Vector{Float64}, Vector{Float64}})
+        @test isa(rpower, Tuple{Vector{Float64}, Vector{Float64}})
+
+    finally
+        @info("removing test files")
+        rm(path, force = true, recursive = true)
+    end
 end
