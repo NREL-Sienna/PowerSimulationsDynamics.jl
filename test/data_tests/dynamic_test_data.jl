36dff2e2d4ad5a386d3c494f2ced271268fd7877
Rodrigo Henriquez <rodrigomha@gmail.com>
2021-06-09 16:13:44 -0700

    add gfoll dynamic test data

--- test/data_tests/dynamic_test_data.jl
+++ test/data_tests/dynamic_test_data.jl
@@ -251,12 +251,19 @@ dc_source_hv() = FixedDCSource(voltage = 1500.0) #Not in the original data, gues
 
 ###### Filter Data ######
 filt() = LCLFilter(lf = 0.08, rf = 0.003, cf = 0.074, lg = 0.2, rg = 0.01)
+filt_gfoll() = LCLFilter(lf = 0.009, rf = 0.016, cf = 2.5, lg = 0.002, rg = 0.003)
 
 ###### PLL Data ######
 pll() = KauraPLL(
     ω_lp = 500.0, #Cut-off frequency for LowPass filter of PLL filter.
     kp_pll = 0.084,  #PLL proportional gain
     ki_pll = 4.69,   #PLL integral gain
+)
+
+reduced_pll() = ReducedOrderPLL(
+    ω_lp = 1.32 * 2 * pi * 50, #Cut-off frequency for LowPass filter of PLL filter.
+    kp_pll = 2.0,  #PLL proportional gain
+    ki_pll = 20.0,   #PLL integral gain
 )
 
 no_pll() = PSY.FixedFrequency()
@@ -292,6 +299,16 @@ function outer_control_droop()
     return OuterControl(active_droop(), reactive_droop())
 end
 
+function outer_control_gfoll()
+    function active_pi()
+        return ActivePowerPI(Kp_p = 2.0, Ki_p = 30.0, ωz = 0.132 * 2 * pi * 50)
+    end
+    function reactive_pi()
+        return ReactivePowerPI(Kp_q = 2.0, Ki_q = 30.0, ωf = 0.132 * 2 * pi * 50.0)
+    end
+    return OuterControl(active_pi(), reactive_pi())
+end
+
 ######## Inner Control ######
 inner_control() = CurrentControl(
     kpv = 0.59,     #Voltage controller proportional gain
@@ -305,6 +322,14 @@ inner_control() = CurrentControl(
     ωad = 50.0,     #Active damping low pass filter cut-off frequency
     kad = 0.2,
 )
+
+current_mode_inner() = CurrentModeControl(
+    kpc = 0.37,     #Current controller proportional gain
+    kic = 0.7,     #Current controller integral gain
+    kffv = 1.0,     #Binary variable enabling the voltage feed-forward in output of current controllers
+)
+
+####### Devices #######
 
 function dyn_gen_second_order(generator)
     return PSY.DynamicGenerator(
