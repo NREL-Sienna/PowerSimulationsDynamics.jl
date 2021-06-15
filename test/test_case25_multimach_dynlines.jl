2cc8d15e28a4bfcd81523d21f89294f3527c516a
Jose Daniel Lara <jdlara@berkeley.edu>
2021-06-15 13:17:41 -0700

    add mass matrix model

--- test/test_case25_multimach_dynlines.jl
+++ test/test_case25_multimach_dynlines.jl
@@ -24,29 +24,60 @@ tspan = (0.0, 40.0);
 gen2 = get_dynamic_injector(get_component(Generator, sys, "generator-102-1"));
 Pref_change = ControlReferenceChange(1.0, gen2, PSID.P_ref_index, 0.9);
 
-path = (joinpath(pwd(), "test-25"))
-!isdir(path) && mkdir(path)
-try
-    #Define Simulation Problem
-    sim = Simulation!(ImplicitModel, sys, path, tspan, Pref_change)
+@testset "Test 25 Marconato with Dynamic Lines ImplicitModel" begin
+    path = (joinpath(pwd(), "test-25"))
+    !isdir(path) && mkdir(path)
+    try
+        #Define Simulation Problem
+        sim = Simulation!(ImplicitModel, sys, path, tspan, Pref_change)
 
-    small_sig = small_signal_analysis(sim)
-    @test small_sig.stable
+        small_sig = small_signal_analysis(sim)
+        @test small_sig.stable
 
-    #Solve problem in equilibrium
-    execute!(sim, Sundials.IDA(), dtmax = 0.001)
+        #Solve problem in equilibrium
+        execute!(sim, Sundials.IDA(), dtmax = 0.001)
 
-    #Obtain data for angles
-    series = get_state_series(sim, ("generator-102-1", :ω))
+        #Obtain data for angles
+        series = get_state_series(sim, ("generator-102-1", :ω))
 
-    diff = [0.0]
-    res = get_init_values_for_comparison(sim)
-    for (k, v) in test25_x0_init
-        diff[1] += LinearAlgebra.norm(res[k] - v)
+        diff = [0.0]
+        res = get_init_values_for_comparison(sim)
+        for (k, v) in test25_x0_init
+            diff[1] += LinearAlgebra.norm(res[k] - v)
+        end
+        @test (diff[1] < 1e-3)
+        @test sim.solution.retcode == :Success
+    finally
+        @info("removing test files")
+        rm(path, force = true, recursive = true)
     end
-    @test (diff[1] < 1e-3)
-    @test sim.solution.retcode == :Success
-finally
-    @info("removing test files")
-    rm(path, force = true, recursive = true)
+end
+
+@testset "Test 25 Marconato with Dynamic Lines MassMatrixModel" begin
+    path = (joinpath(pwd(), "test-25"))
+    !isdir(path) && mkdir(path)
+    try
+        #Define Simulation Problem
+        sim = Simulation!(MassMatrixModel, sys, path, tspan, Pref_change)
+
+        small_sig = small_signal_analysis(sim)
+        @test small_sig.stable
+
+        #Solve problem in equilibrium
+        execute!(sim, Rodas5(autodiff = false), dtmax = 0.001)
+
+        #Obtain data for angles
+        series = get_state_series(sim, ("generator-102-1", :ω))
+
+        diff = [0.0]
+        res = get_init_values_for_comparison(sim)
+        for (k, v) in test25_x0_init
+            diff[1] += LinearAlgebra.norm(res[k] - v)
+        end
+        @test (diff[1] < 1e-3)
+        @test sim.solution.retcode == :Success
+    finally
+        @info("removing test files")
+        rm(path, force = true, recursive = true)
+    end
 end
