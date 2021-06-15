453051070f960e2b771d3c0ab5f31dcb5a7de147
Jose Daniel Lara <jdlara@berkeley.edu>
2021-06-15 13:09:18 -0700

    update test code

--- test/data_tests/test25.jl
+++ test/data_tests/test25.jl
@@ -5,25 +5,48 @@ const PSY = PowerSystems
 include(joinpath(dirname(@__FILE__), "dynamic_test_data.jl"))
 include(joinpath(dirname(@__FILE__), "data_utils.jl"))
 ############### Data Network ########################
-omib_file_dir = joinpath(dirname(@__FILE__), "OMIB_DARCO_PSR.raw")
-omib_sys = System(omib_file_dir, runchecks = false)
-add_source_to_ref(omib_sys)
+sys_dir = joinpath(dirname(@__FILE__), "ThreeBusMultiLoad.raw")
+sys = System(sys_dir, runchecks = false)
 
 ############### Data Dynamic devices ########################
-function inv_gfoll(static_device)
-    return PSY.DynamicInverter(
-        get_name(static_device),
-        1.0, #ω_ref
-        converter_low_power(), #converter
-        outer_control_gfoll(), #outercontrol
-        current_mode_inner(), #inner_control
-        dc_source_lv(),
-        reduced_pll(),
-        filt_gfoll(),
-    ) #pss
+function dyn_gen_marconato(generator)
+    return PSY.DynamicGenerator(
+        name = get_name(generator),
+        ω_ref = 1.0, # ω_ref,
+        machine = machine_marconato(), #machine
+        shaft = shaft_damping(), #shaft
+        avr = avr_none(), #avr
+        prime_mover = tg_none(), #tg
+        pss = pss_none(),
+    )
 end
 
-#Attach dynamic generator. Currently use PSS/e format based on bus #.
-device = [g for g in get_components(Generator, omib_sys)][1]
-case_inv = inv_gfoll(device)
-add_component!(omib_sys, case_inv, device)
+function dyn_gen_marconato_tg(generator)
+    return PSY.DynamicGenerator(
+        name = get_name(generator),
+        ω_ref = 1.0, # ω_ref,
+        machine = machine_marconato(), #machine
+        shaft = shaft_damping(), #shaft
+        avr = avr_none(), #avr
+        prime_mover = tg_type2(), #tg
+        pss = pss_none(),
+    )
+end
+
+# Add dynamic generators to the system (each gen is linked through a static one)
+for g in get_components(Generator, sys)
+    if get_number(get_bus(g)) == 101
+        case_gen = dyn_gen_marconato_tg(g)
+        add_component!(sys, case_gen, g)
+    elseif get_number(get_bus(g)) == 102
+        case_gen = dyn_gen_marconato(g)
+        add_component!(sys, case_gen, g)
+    end
+end
+
+# Transform all lines into dynamic lines
+# Not recommended approach since it will remove static lines while going through the loop
+for line in get_components(Line, sys)
+    dyn_line = DynamicBranch(line)
+    add_component!(sys, dyn_line)
+end
--- test/test_case25_multimach_dynlines.jl
+++ test/test_case25_multimach_dynlines.jl
@@ -2,7 +2,7 @@ using PowerSimulationsDynamics
 using Sundials
 
 """
-Case 24:
+Case 25:
 This case study a three-bus system, with two Marconato generators (in buses 1 and 2), and a constant impedance load in bus 3.
 The perturbation increase the reference of mechanical power of generator-2 from 0.8 to 0.9 at t=1.0s.
 """
@@ -11,7 +11,7 @@ The perturbation increase the reference of mechanical power of generator-2 from 
 ############### LOAD DATA ########################
 ##################################################
 
-include(joinpath(dirname(@__FILE__), "data_tests/test24.jl"))
+include(joinpath(dirname(@__FILE__), "data_tests/test25.jl"))
 
 ##################################################
 ############### SOLVE PROBLEM ####################
@@ -24,13 +24,14 @@ tspan = (0.0, 40.0);
 gen2 = get_dynamic_injector(get_component(Generator, sys, "generator-102-1"));
 Pref_change = ControlReferenceChange(1.0, gen2, PSID.P_ref_index, 0.9);
 
-path = (joinpath(pwd(), "test-23"))
+path = (joinpath(pwd(), "test-25"))
 !isdir(path) && mkdir(path)
 try
     #Define Simulation Problem
-    sim = Simulation!(path, sys, tspan, Pref_change)
+    sim = Simulation!(ImplicitModel, sys, path, tspan, Pref_change)
 
     small_sig = small_signal_analysis(sim)
+    @test small_sig.stable
 
     #Solve problem in equilibrium
     execute!(sim, Sundials.IDA(), dtmax = 0.001)
@@ -40,12 +41,11 @@ try
 
     diff = [0.0]
     res = get_init_values_for_comparison(sim)
-    for (k, v) in test24_x0_init
+    for (k, v) in test25_x0_init
         diff[1] += LinearAlgebra.norm(res[k] - v)
     end
     @test (diff[1] < 1e-3)
     @test sim.solution.retcode == :Success
-    @test small_sig.stable
 finally
     @info("removing test files")
     rm(path, force = true, recursive = true)
