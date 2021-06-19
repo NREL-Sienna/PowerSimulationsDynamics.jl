4a196161357ed0d27cfc3d2c804f7e9bee184daa
Jose Daniel Lara <jdlara@berkeley.edu>
2021-06-18 17:39:28 -0700

    add methods to avoid issues

--- src/base/supplemental_accesors.jl
+++ src/base/supplemental_accesors.jl
@@ -1,1 +1,13 @@
 get_inner_vars(dynamic_device::PSY.DynamicInjection) = dynamic_device.ext[INNER_VARS]
+
+function _get_value(v::ForwardDiff.Dual)
+    return v.value
+end
+
+function set_inner_vars(dynamic_device, VAR, value::Float64)
+    get_inner_vars(dynamic_device)[VAR] = value
+end
+
+function set_inner_vars(dynamic_device, VAR, value)
+    set_inner_vars(dynamic_device, VAR, _get_value(value))
+end
