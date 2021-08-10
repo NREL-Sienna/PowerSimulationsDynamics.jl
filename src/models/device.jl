3ef9c9a49f3301d6eaf35ea827db9c246dacb741
Matt <67015312+m-bossart@users.noreply.github.com>
2021-08-10 09:58:50 -0600

    change location of functions

--- src/models/device.jl
+++ src/models/device.jl
@@ -214,6 +214,16 @@ function device!(
     return
 end
 
+function device_mass_matrix_entries!(
+    sim_inputs::SimulationInputs,
+    dynamic_device::PSY.PeriodicVariableSource,
+)
+    mass_matrix = get_mass_matrix(sim_inputs)
+    global_index = get_global_index(sim_inputs)[PSY.get_name(dynamic_device)]
+    mass_matrix_pvs_entries!(mass_matrix, dynamic_device, global_index)
+    return
+end
+
 function mass_matrix_pvs_entries!(
     mass_matrix,
     pvs::PSY.PeriodicVariableSource,
@@ -222,12 +232,47 @@ function mass_matrix_pvs_entries!(
     @debug "Using default mass matrix entries $pvs"
 end
 
-function device_mass_matrix_entries!(
-    sim_inputs::SimulationInputs,
+function device!(
+    x,
+    output_ode::Vector{T},
+    voltage_r,
+    voltage_i,
+    current_r,
+    current_i,
+    ix_range::UnitRange{Int},
+    ode_range::UnitRange{Int},
     dynamic_device::PSY.PeriodicVariableSource,
-)
-    mass_matrix = get_mass_matrix(sim_inputs)
-    global_index = get_global_index(sim_inputs)[PSY.get_name(dynamic_device)]
-    mass_matrix_pvs_entries!(mass_matrix, dynamic_device, global_index)
+    inputs::SimulationInputs,
+    t,
+) where {T <: Real}
+    #Obtain local device states
+    internal_states = @view x[ix_range]
+
+    ω_θ = PSY.get_internal_angle_frequencies(dynamic_device)
+    ω_V = PSY.get_internal_angle_frequencies(dynamic_device)
+    output_ode[ode_range][1] = dV = 0
+    for (ix, A) in enumerate(PSY.get_internal_voltage_coefficients(dynamic_device))
+        t <= 0 && continue
+        dV += ω_V[ix] * (A[1] * cos(ω_V[ix] * t) - A[2] * sin(ω_V[ix] * t))
+    end
+
+    output_ode[ode_range][2] = dθ = 0
+    for (ix, A) in enumerate(PSY.get_internal_angle_coefficients(dynamic_device))
+        t <= 0 && continue
+        dθ += ω_θ[ix] * (A[1] * cos(ω_θ[ix] * t) - A[2] * sin(ω_θ[ix] * t))
+    end
+
+    # Internal Voltage states
+    V_R = internal_states[1] * cos(internal_states[2])
+    V_I = internal_states[1] * sin(internal_states[2])
+
+    R_th = PSY.get_R_th(dynamic_device)
+    X_th = PSY.get_X_th(dynamic_device)
+    Zmag = R_th^2 + X_th^2
+
+    #update current
+    current_r[1] += R_th * (V_R - voltage_r[1]) / Zmag + X_th * (V_I - voltage_i[1]) / Zmag #in system pu flowing out
+    current_i[1] += R_th * (V_I - voltage_i[1]) / Zmag - X_th * (V_R - voltage_r[1]) / Zmag #in system pu flowing out
+
     return
 end
