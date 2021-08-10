de65afa6ab7cfc0201fbe0a3059866e10c2073b5
Matt <67015312+m-bossart@users.noreply.github.com>
2021-08-10 11:41:21 -0600

    @view for local output_ode

--- src/models/device.jl
+++ src/models/device.jl
@@ -247,16 +247,17 @@ function device!(
 ) where {T <: Real}
     #Obtain local device states
     internal_states = @view x[ix_range]
-
+    internal_ode = @view output_ode[ode_range]
     ω_θ = PSY.get_internal_angle_frequencies(dynamic_device)
     ω_V = PSY.get_internal_angle_frequencies(dynamic_device)
-    output_ode[ode_range][1] = dV = 0
+
+    dV = 0
     for (ix, A) in enumerate(PSY.get_internal_voltage_coefficients(dynamic_device))
         t <= 0 && continue
         dV += ω_V[ix] * (A[1] * cos(ω_V[ix] * t) - A[2] * sin(ω_V[ix] * t))
     end
 
-    output_ode[ode_range][2] = dθ = 0
+    dθ = 0
     for (ix, A) in enumerate(PSY.get_internal_angle_coefficients(dynamic_device))
         t <= 0 && continue
         dθ += ω_θ[ix] * (A[1] * cos(ω_θ[ix] * t) - A[2] * sin(ω_θ[ix] * t))
@@ -265,12 +266,13 @@ function device!(
     # Internal Voltage states
     V_R = internal_states[1] * cos(internal_states[2])
     V_I = internal_states[1] * sin(internal_states[2])
+    internal_ode[1] = dV
+    internal_ode[2] = dθ
 
+    #update current
     R_th = PSY.get_R_th(dynamic_device)
     X_th = PSY.get_X_th(dynamic_device)
     Zmag = R_th^2 + X_th^2
-
-    #update current
     current_r[1] += R_th * (V_R - voltage_r[1]) / Zmag + X_th * (V_I - voltage_i[1]) / Zmag #in system pu flowing out
     current_i[1] += R_th * (V_I - voltage_i[1]) / Zmag - X_th * (V_R - voltage_r[1]) / Zmag #in system pu flowing out
 
