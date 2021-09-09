24f70d0703fd7ec1b4a994ec6e3a481128f1f427
Jose Daniel Lara <jdlara@berkeley.edu>
2021-09-08 17:35:14 -0700

    update generic functions

--- src/models/branch.jl
+++ src/models/branch.jl
@@ -1,6 +1,5 @@
 function branch!(
-    x,
-    dx,
+    device_states,
     output_ode::Vector{T},
     V_r_from,
     V_i_from,
@@ -10,13 +9,10 @@ function branch!(
     current_i_from,
     current_r_to,
     current_i_to,
-    ix_range::UnitRange{Int},
-    ode_range::UnitRange{Int},
     branch::PSY.DynamicBranch,
     inputs::SimulationInputs,
 ) where {T <: Real}
     #Obtain local device states
-    device_states = @view x[ix_range]
 
     #Obtain references
     sys_f = get_base_frequency(inputs)
