421069f8c165c2fc519dda8e3fc11cbca25029fa
Jose Daniel Lara <jdlara@berkeley.edu>
2021-09-08 17:35:01 -0700

    update functions for static devices

--- src/models/load_models.jl
+++ src/models/load_models.jl
@@ -1,13 +1,17 @@
-function mdl_Zload!(voltage_r, voltage_i, current_r, current_i, device::PSY.PowerLoad)
-    bus_ix = get_bus_ix(static_device)
+function mdl_Zload!(
+    voltage_r::T,
+    voltage_i::T,
+    current_r::AbstractArray{T},
+    current_i::AbstractArray{T},
+    static_device::PSY.PowerLoad,
+) where {T <: Real}
     #Load squared voltage magnitude at steady state
-    bus = PSY.get_bus(device)
-    Vmag_sq = PSY.get_magnitude(bus)^2
+    Vmag_sq = PSY.get_magnitude(PSY.get_bus(static_device))^2
     #Vmag_sq = 1.0
 
     #Load device parameters
-    P = PSY.get_active_power(device)
-    Q = PSY.get_reactive_power(device)
+    P = PSY.get_active_power(static_device)
+    Q = PSY.get_reactive_power(static_device)
 
     #Compute impedance and load: Z = |V|^2/conj(S)
     #Z_load =  Vmag_sq/(P-Q*1im) #in pu
@@ -17,8 +21,8 @@ function mdl_Zload!(voltage_r, voltage_i, current_r, current_i, device::PSY.Powe
 
     #Update current
     #This model creates an equivalent RL/RC circuit based on steady state voltage
-    current_r[ix] += -(1.0 / Vmag_sq) * (voltage_r[ix] * P + voltage_i[ix] * Q) #in system pu flowing out
-    current_i[ix] += -(1.0 / Vmag_sq) * (voltage_i[ix] * P - voltage_r[ix] * Q) #in system pu flowing out
+    current_r[1] += -(1.0 / Vmag_sq) * (voltage_r * P + voltage_i * Q) #in system pu flowing out
+    current_i[1] += -(1.0 / Vmag_sq) * (voltage_i * P - voltage_r * Q) #in system pu flowing out
 
     return
 end
--- src/models/source_models.jl
+++ src/models/source_models.jl
@@ -1,24 +1,21 @@
 function mdl_source!(
-    voltage_r,
-    voltage_i,
-    current_r,
-    current_i,
-    device::StaticWrapper{PSY.Source},
-)
+    voltage_r::T,
+    voltage_i::T,
+    current_r::AbstractArray{T},
+    current_i::AbstractArray{T},
+    static_device::StaticWrapper{PSY.Source},
+) where {T <: Real}
     #Load device parameters
-    bus_ix = get_bus_ix(device)
-    V_R = get_V_ref(device) * cos(get_θ_ref(device))
-    V_I = get_V_ref(device) * sin(get_θ_ref(device))
+    V_R = get_V_ref(static_device) * cos(get_θ_ref(static_device))
+    V_I = get_V_ref(static_device) * sin(get_θ_ref(static_device))
     # TODO: field forwarding
-    R_th = PSY.get_R_th(device.device)
-    X_th = PSY.get_X_th(device.device)
+    R_th = PSY.get_R_th(static_device.device)
+    X_th = PSY.get_X_th(static_device.device)
     Zmag = R_th^2 + X_th^2
 
     #update current
-    current_r[bus_ix] +=
-        R_th * (V_R - voltage_r[bus_ix]) / Zmag + X_th * (V_I - voltage_i[bus_ix]) / Zmag #in system pu flowing out
-    current_i[bus_ix] +=
-        R_th * (V_I - voltage_i[bus_ix]) / Zmag - X_th * (V_R - voltage_r[bus_ix]) / Zmag #in system pu flowing out
+    current_r[1] += R_th * (V_R - voltage_r) / Zmag + X_th * (V_I - voltage_i) / Zmag #in system pu flowing out
+    current_i[1] += R_th * (V_I - voltage_i) / Zmag - X_th * (V_R - voltage_r) / Zmag #in system pu flowing out
 
     return
 end
