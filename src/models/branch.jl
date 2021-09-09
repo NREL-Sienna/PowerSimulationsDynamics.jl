f979d3801c8cff88e491c00c1748d5b3c0a2c8b8
Jose Daniel Lara <jdlara@berkeley.edu>
2021-09-09 16:56:42 -0700

    update branches

--- src/models/branch.jl
+++ src/models/branch.jl
@@ -1,34 +1,28 @@
 function branch!(
-    device_states,
-    output_ode::Vector{T},
-    V_r_from,
-    V_i_from,
-    V_r_to,
-    V_i_to,
-    current_r_from,
-    current_i_from,
-    current_r_to,
-    current_i_to,
-    branch::PSY.DynamicBranch,
-    inputs::SimulationInputs,
+    device_states::AbstractArray{T},
+    output_ode::AbstractArray{T},
+    voltage_r_from::T,
+    voltage_i_from::T,
+    voltage_r_to::T,
+    voltage_i_to::T,
+    current_r_from::AbstractArray{T},
+    current_i_from::AbstractArray{T},
+    current_r_to::AbstractArray{T},
+    current_i_to::AbstractArray{T},
+    branch::BranchWrapper,
 ) where {T <: Real}
-    #Obtain local device states
-
-    #Obtain references
-    sys_f = get_base_frequency(inputs)
-
     mdl_line_ode!(
         device_states,
-        view(output_ode, ode_range),
-        V_r_from,
-        V_i_from,
-        V_r_to,
-        V_i_to,
+        output_ode,
+        voltage_r_from,
+        voltage_i_from,
+        voltage_r_to,
+        voltage_i_to,
         current_r_from,
         current_i_from,
         current_r_to,
         current_i_to,
-        sys_f,
+        # TODO: pass wrapper
         branch,
     )
 
--- src/models/dynline_model.jl
+++ src/models/dynline_model.jl
@@ -1,25 +1,24 @@
 function mdl_line_ode!(
-    device_states,
-    output_ode,
-    V_r_from,
-    V_i_from,
-    V_r_to,
-    V_i_to,
-    current_r_from,
-    current_i_from,
-    current_r_to,
-    current_i_to,
-    sys_f,
-    branch::PSY.DynamicBranch,
-)
+    device_states::AbstractArray{T},
+    output_ode::AbstractArray{T},
+    voltage_r_from::T,
+    voltage_i_from::T,
+    voltage_r_to::T,
+    voltage_i_to::T,
+    current_r_from::AbstractArray{T},
+    current_i_from::AbstractArray{T},
+    current_r_to::AbstractArray{T},
+    current_i_to::AbstractArray{T},
+    branch::BranchWrapper,
+) where {T <: Real}
     L = PSY.get_x(branch)
     R = PSY.get_r(branch)
-    ω_b = sys_f * 2 * π
+    ω_b = get_system_base_frequency(branch) * 2 * π
 
     Il_r = device_states[1]
     Il_i = device_states[2]
-    output_ode[1] = (ω_b / L) * ((V_r_from[1] - V_r_to[1]) - (R * Il_r - L * Il_i))
-    output_ode[2] = (ω_b / L) * ((V_i_from[1] - V_i_to[1]) - (R * Il_i + L * Il_r))
+    output_ode[1] = (ω_b / L) * ((voltage_r_from - voltage_r_to) - (R * Il_r - L * Il_i))
+    output_ode[2] = (ω_b / L) * ((voltage_i_from - voltage_i_to) - (R * Il_i + L * Il_r))
 
     current_r_from[1] -= Il_r
     current_i_from[1] -= Il_i
