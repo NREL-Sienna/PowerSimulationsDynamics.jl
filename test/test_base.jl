96910900ccd5f996bb03dcd073c95c185387a84b
Jose Daniel Lara <jdlara@berkeley.edu>
2021-09-09 21:28:29 -0700

    update network perturbation and test

--- test/test_base.jl
+++ test/test_base.jl
@@ -221,4 +221,20 @@ end
         @test isapprox(inputs.ybus_rectangular[i + 3, j], -imag(complex_ybus), atol = 1e-10)
         @test isapprox(inputs.ybus_rectangular[i, j + 3], imag(complex_ybus), atol = 1e-10)
     end
+
+    threebus_sys = System(three_bus_file_dir, runchecks = false)
+    ybus_original = PSY.Ybus(threebus_sys)
+    cb1 = NetworkSwitch(1.0, ybus_original)
+
+    @test all(
+        cb1.ybus_rectangular .== NetworkSwitch(1.0, ybus_original.data).ybus_rectangular,
+    )
+
+    for i in 1:3, j in 1:3
+        complex_ybus = ybus_original.data[i, j]
+        @test cb1.ybus_rectangular[i, j] == real(complex_ybus)
+        @test cb1.ybus_rectangular[i + 3, j + 3] == real(complex_ybus)
+        @test cb1.ybus_rectangular[i + 3, j] == -imag(complex_ybus)
+        @test cb1.ybus_rectangular[i, j + 3] == imag(complex_ybus)
+    end
 end
