9c906e328ddced6e12899a3d9a60f5aa1131866f
Rodrigo Henriquez <rodrigomha@gmail.com>
2021-09-13 16:40:27 -0700

    add test 20

--- test/test_case20_ac1a.jl
+++ test/test_case20_ac1a.jl
@@ -42,20 +42,35 @@ function test_ac1a_implicit(dyr_file, csv_file, init_cond, eigs_value)
             sys, #system
             path,
             tspan, #time span
-            BranchTrip(1.0, "BUS 1-BUS 2-i_1"), #Type of Fault
+            BranchTrip(1.0, PSY.Line, "BUS 1-BUS 2-i_1"), #Type of Fault
         ) #Type of Fault
 
-        #Solve problem
-        execute!(sim, IDA(), dtmax = 0.005, saveat = 0.005)
+        #Test Initial Condition
+        diff = [0.0]
+        res = get_init_values_for_comparison(sim)
+        for (k, v) in init_cond
+            diff[1] += LinearAlgebra.norm(res[k] - v)
+        end
 
-        small_sig = small_signal_analysis(sim; reset_simulation = true)
+        @test (diff[1] < 1e-3)
+
+        # Obtain small signal results for initial conditions
+        small_sig = small_signal_analysis(sim)
         eigs = small_sig.eigenvalues
         @test small_sig.stable
 
+        #Test Eigenvalues
+        @test LinearAlgebra.norm(eigs - eigs_value) < 1e-3
+
+        #Solve problem
+        @test execute!(sim, IDA(), dtmax = 0.005, saveat = 0.005) ==
+              PSID.SIMULATION_FINALIZED
+        results = read_results(sim)
+
         #Obtain data for angles
-        series = get_state_series(res, ("generator-102-1", :δ))
+        series = get_state_series(results, ("generator-102-1", :δ))
         #Obtain data for voltage magnitude at bus 102
-        series2 = get_voltage_magnitude_series(res, 102)
+        series2 = get_voltage_magnitude_series(results, 102)
         t = series[1]
         δ = series[2]
         V = series2[2]
@@ -64,23 +79,11 @@ function test_ac1a_implicit(dyr_file, csv_file, init_cond, eigs_value)
         t_psse, δ_psse = clean_extra_timestep!(M[:, 1], M[:, 2])
         _, V_psse = clean_extra_timestep!(M[:, 1], M[:, 3])
 
-        diff = [0.0]
-        res = get_init_values_for_comparison(sim)
-        for (k, v) in init_cond
-            diff[1] += LinearAlgebra.norm(res[k] - v)
-        end
-        #Test Initial Condition
-        @test (diff[1] < 1e-3)
-        #Test Eigenvalues
-        @test LinearAlgebra.norm(eigs - eigs_value) < 1e-3
-        #Test Solution DiffEq
-        @test res.solution.retcode == :Success
         #Test Transient Simulation Results
         # PSSE results are in Degrees
         @test LinearAlgebra.norm(δ - (δ_psse .* pi / 180), Inf) <= 1e-2
         @test LinearAlgebra.norm(V - V_psse, Inf) <= 1e-1
         @test LinearAlgebra.norm(t - round.(t_psse, digits = 3)) == 0.0
-
     finally
         @info("removing test files")
         rm(path, force = true, recursive = true)
@@ -99,21 +102,35 @@ function test_ac1a_mass_matrix(dyr_file, csv_file, init_cond, eigs_value)
             sys, #system
             path,
             tspan, #time span
-            BranchTrip(1.0, "BUS 1-BUS 2-i_1"), #Type of Fault
+            BranchTrip(1.0, PSY.Line, "BUS 1-BUS 2-i_1"), #Type of Fault
         ) #Type of Fault
 
-        #Solve problem
-        execute!(sim, Rodas5(autodiff = true), dtmax = 0.005, saveat = 0.005)
+        #Test Initial Condition
+        diff = [0.0]
+        res = get_init_values_for_comparison(sim)
+        for (k, v) in init_cond
+            diff[1] += LinearAlgebra.norm(res[k] - v)
+        end
 
-        #Obtain small signal results for initial conditions. Testing the simulation reset
-        small_sig = small_signal_analysis(sim; reset_simulation = true)
+        @test (diff[1] < 1e-3)
+
+        # Obtain small signal results for initial conditions
+        small_sig = small_signal_analysis(sim)
         eigs = small_sig.eigenvalues
         @test small_sig.stable
 
+        #Test Eigenvalues
+        @test LinearAlgebra.norm(eigs - eigs_value) < 1e-3
+
+        #Solve problem
+        @test execute!(sim, Rodas4(), dtmax = 0.005, saveat = 0.005) ==
+              PSID.SIMULATION_FINALIZED
+        results = read_results(sim)
+
         #Obtain data for angles
-        series = get_state_series(res, ("generator-102-1", :δ))
+        series = get_state_series(results, ("generator-102-1", :δ))
         #Obtain data for voltage magnitude at bus 102
-        series2 = get_voltage_magnitude_series(res, 102)
+        series2 = get_voltage_magnitude_series(results, 102)
         t = series[1]
         δ = series[2]
         V = series2[2]
@@ -122,23 +139,11 @@ function test_ac1a_mass_matrix(dyr_file, csv_file, init_cond, eigs_value)
         t_psse, δ_psse = clean_extra_timestep!(M[:, 1], M[:, 2])
         _, V_psse = clean_extra_timestep!(M[:, 1], M[:, 3])
 
-        diff = [0.0]
-        res = get_init_values_for_comparison(sim)
-        for (k, v) in init_cond
-            diff[1] += LinearAlgebra.norm(res[k] - v)
-        end
-        #Test Initial Condition
-        @test (diff[1] < 1e-3)
-        #Test Eigenvalues
-        @test LinearAlgebra.norm(eigs - eigs_value) < 1e-3
-        #Test Solution DiffEq
-        @test res.solution.retcode == :Success
         #Test Transient Simulation Results
         # PSSE results are in Degrees
         @test LinearAlgebra.norm(δ - (δ_psse .* pi / 180), Inf) <= 1e-2
         @test LinearAlgebra.norm(V - V_psse, Inf) <= 1e-1
         @test LinearAlgebra.norm(t - round.(t_psse, digits = 3)) == 0.0
-
     finally
         @info("removing test files")
         rm(path, force = true, recursive = true)
