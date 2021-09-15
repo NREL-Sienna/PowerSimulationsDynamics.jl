52e48b395d084cf2ae8b391ea16253e1a6c7c795
Rodrigo Henriquez <rodrigomha@gmail.com>
2021-09-15 14:05:30 -0700

    fix test 16

--- test/test_case16_genroe.jl
+++ test/test_case16_genroe.jl
@@ -35,61 +35,55 @@ function test_genroe_implicit(dyr_file, csv_file, init_cond, eigs_value)
     try
         sys = System(raw_file_dir, dyr_file)
 
-        #Compute Ybus_fault
-        sys2 = System(raw_file_dir)
-        #Remove line connecting bus 1 to 2.
-        remove_component!(Line, sys2, "BUS 1-BUS 2-i_1")
-        Ybus_fault = Ybus(sys2).data
-
-        #Define Fault: Change of YBus
-        Ybus_change = NetworkSwitch(
-            1.0, #change at t = 1.0
-            Ybus_fault, #New YBus
-        )
-
-        #Define Simulation Problem
+        # Define Simulation Problem
         sim = Simulation!(
             ResidualModel,
             sys, #system
             path,
             tspan, #time span
-            Ybus_change,
+            BranchTrip(1.0, PSY.Line, "BUS 1-BUS 2-i_1"), #Type of Fault
         ) #Type of Fault
 
-        #Obtain small signal results for initial conditions
-        small_sig = small_signal_analysis(sim)
-        eigs = small_sig.eigenvalues
-        @test small_sig.stable
-
-        #Solve problem
-        execute!(sim, IDA(), dtmax = 0.005, saveat = 0.005)
-
-        #Obtain data for angles
-        series = get_state_series(res, ("generator-102-1", :δ))
-        t = series[1]
-        δ = series[2]
-
-        series2 = get_voltage_magnitude_series(res, 102)
-
-        t_psse, δ_psse = get_csv_delta(csv_file)
-
+        # Test Initial Condition
         diff = [0.0]
         res = get_init_values_for_comparison(sim)
         for (k, v) in init_cond
             diff[1] += LinearAlgebra.norm(res[k] - v)
         end
-        #Test Initial Condition
+
         @test (diff[1] < 1e-3)
+
+        # Obtain small signal results for initial conditions
+        small_sig = small_signal_analysis(sim)
+        eigs = small_sig.eigenvalues
+        @test small_sig.stable
+
         #Test Eigenvalues
         @test LinearAlgebra.norm(eigs - eigs_value) < 1e-3
-        #Test Solution DiffEq
-        @test res.solution.retcode == :Success
 
-        #Test Transient Simulation Results
+        # Solve problem
+        @test execute!(sim, IDA(), dtmax = 0.005, saveat = 0.005) ==
+              PSID.SIMULATION_FINALIZED
+        results = read_results(sim)
+
+        #Obtain data for angles
+        series = get_state_series(results, ("generator-102-1", :δ))
+        t = series[1]
+        δ = series[2]
+        series2 = get_voltage_magnitude_series(results, 102)
+
+        t_psse, δ_psse = get_csv_delta(csv_file)
+
+        # Test Transient Simulation Results
         # PSSE results are in Degrees
         @test LinearAlgebra.norm(δ - (δ_psse .* pi / 180), Inf) <= 1e-1
         @test LinearAlgebra.norm(t - round.(t_psse, digits = 3)) == 0.0
 
+        power = PSID.get_activepower_series(results, "generator-102-1")
+        rpower = PSID.get_reactivepower_series(results, "generator-102-1")
+        @test isa(series2, Tuple{Vector{Float64}, Vector{Float64}})
+        @test isa(power, Tuple{Vector{Float64}, Vector{Float64}})
+        @test isa(rpower, Tuple{Vector{Float64}, Vector{Float64}})
     finally
         @info("removing test files")
         rm(path, force = true, recursive = true)
@@ -102,60 +96,55 @@ function test_genroe_mass_matrix(dyr_file, csv_file, init_cond, eigs_value)
     try
         sys = System(raw_file_dir, dyr_file)
 
-        #Compute Ybus_fault
-        sys2 = System(raw_file_dir)
-        #Remove line connecting bus 1 to 2.
-        remove_component!(Line, sys2, "BUS 1-BUS 2-i_1")
-        Ybus_fault = Ybus(sys2).data
-
-        #Define Fault: Change of YBus
-        Ybus_change = NetworkSwitch(
-            1.0, #change at t = 1.0
-            Ybus_fault, #New YBus
-        )
-
-        #Define Simulation Problem
+        # Define Simulation Problem
         sim = Simulation!(
             MassMatrixModel,
             sys, #system
             path,
             tspan, #time span
-            Ybus_change,
+            BranchTrip(1.0, PSY.Line, "BUS 1-BUS 2-i_1"), #Type of Fault
         ) #Type of Fault
 
-        #Obtain small signal results for initial conditions
-        small_sig = small_signal_analysis(sim)
-        eigs = small_sig.eigenvalues
-        @test small_sig.stable
-
-        #Solve problem
-        execute!(sim, Rodas5(), dtmax = 0.005, saveat = 0.005)
-
-        #Obtain data for angles
-        series = get_state_series(res, ("generator-102-1", :δ))
-        t = series[1]
-        δ = series[2]
-
-        series2 = get_voltage_magnitude_series(res, 102)
-
-        t_psse, δ_psse = get_csv_delta(csv_file)
-
+        # Test Initial Condition
         diff = [0.0]
         res = get_init_values_for_comparison(sim)
         for (k, v) in init_cond
             diff[1] += LinearAlgebra.norm(res[k] - v)
         end
-        #Test Initial Condition
+
         @test (diff[1] < 1e-3)
+
+        # Obtain small signal results for initial conditions
+        small_sig = small_signal_analysis(sim)
+        eigs = small_sig.eigenvalues
+        @test small_sig.stable
+
         #Test Eigenvalues
         @test LinearAlgebra.norm(eigs - eigs_value) < 1e-3
-        #Test Solution DiffEq
-        @test res.solution.retcode == :Success
-        #Test Transient Simulation Results
+
+        # Solve problem
+        @test execute!(sim, Rodas4(), dtmax = 0.005, saveat = 0.005) ==
+              PSID.SIMULATION_FINALIZED
+        results = read_results(sim)
+
+        #Obtain data for angles
+        series = get_state_series(results, ("generator-102-1", :δ))
+        t = series[1]
+        δ = series[2]
+        series2 = get_voltage_magnitude_series(results, 102)
+
+        t_psse, δ_psse = get_csv_delta(csv_file)
+
+        # Test Transient Simulation Results
         # PSSE results are in Degrees
         @test LinearAlgebra.norm(δ - (δ_psse .* pi / 180), Inf) <= 1e-1
         @test LinearAlgebra.norm(t - round.(t_psse, digits = 3)) == 0.0
 
+        power = PSID.get_activepower_series(results, "generator-102-1")
+        rpower = PSID.get_reactivepower_series(results, "generator-102-1")
+        @test isa(series2, Tuple{Vector{Float64}, Vector{Float64}})
+        @test isa(power, Tuple{Vector{Float64}, Vector{Float64}})
+        @test isa(rpower, Tuple{Vector{Float64}, Vector{Float64}})
     finally
         @info("removing test files")
         rm(path, force = true, recursive = true)
