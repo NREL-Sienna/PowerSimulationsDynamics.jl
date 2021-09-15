4e2d27b7b8aa30b121a5a0e8edd0275bb78592da
Rodrigo Henriquez <rodrigomha@gmail.com>
2021-09-15 13:53:07 -0700

    fix test 14

--- test/test_case14_inverter_ref.jl
+++ test/test_case14_inverter_ref.jl
@@ -24,11 +24,12 @@ Ybus_change = NetworkSwitch(
     Ybus_fault,
 ) #New YBus
 
-@testset "Test 13 Inverter Ref ResidualModel" begin
+@testset "Test 14 Inverter Ref ResidualModel" begin
     path = (joinpath(pwd(), "test-14"))
     !isdir(path) && mkdir(path)
     try
-        sim = Simulation!(
+        # Define Simulation Problem
+        sim = Simulation(
             ResidualModel,
             threebus_sys, #system,
             path,
@@ -36,38 +37,44 @@ Ybus_change = NetworkSwitch(
             Ybus_change, #Type of Fault
         )
 
-        small_sig = small_signal_analysis(sim)
-        eigs = small_sig.eigenvalues
-        @test small_sig.stable
-
-        #Run simulation
-        execute!(
-            sim, #simulation structure
-            IDA(),#Sundials DAE Solver
-            dtmax = 0.001, #keywords arguments
-        )
-
-        series = get_state_series(res, ("generator-101-1", :ω_oc))
-
+        # Test Initial Condition
         diff = [0.0]
         res = get_init_values_for_comparison(sim)
         for (k, v) in test14_x0_init
             diff[1] += LinearAlgebra.norm(res[k] - v)
         end
+
         @test (diff[1] < 1e-3)
+
+        # Obtain small signal results for initial conditions
+        small_sig = small_signal_analysis(sim)
+        eigs = small_sig.eigenvalues
+        @test small_sig.stable
+
+        # Test Eigenvalues
         @test LinearAlgebra.norm(eigs - test14_eigvals) < 1e-3
-        @test res.solution.retcode == :Success
+
+        #Run simulation
+        @test execute!(
+            sim, #simulation structure
+            IDA(),#Sundials DAE Solver
+            dtmax = 0.001, #keywords arguments
+        ) == PSID.SIMULATION_FINALIZED
+        results = read_results(sim)
+
+        series = get_state_series(results, ("generator-101-1", :ω_oc))
     finally
         @info("removing test files")
         rm(path, force = true, recursive = true)
     end
 end
 
-@testset "Test 13 Inverter Ref MassMatrixModel" begin
+@testset "Test 14 Inverter Ref MassMatrixModel" begin
     path = (joinpath(pwd(), "test-14"))
     !isdir(path) && mkdir(path)
     try
-        sim = Simulation!(
+        # Define Simulation Problem
+        sim = Simulation(
             MassMatrixModel,
             threebus_sys, #system,
             path,
@@ -75,27 +82,32 @@ end
             Ybus_change, #Type of Fault
         )
 
-        small_sig = small_signal_analysis(sim)
-        eigs = small_sig.eigenvalues
-        @test small_sig.stable
-
-        #Run simulation
-        execute!(
-            sim, #simulation structure
-            Rodas5(),#Sundials DAE Solver
-            dtmax = 0.001, #keywords arguments
-        )
-
-        series = get_state_series(res, ("generator-101-1", :ω_oc))
-
+        # Test Initial Condition
         diff = [0.0]
         res = get_init_values_for_comparison(sim)
         for (k, v) in test14_x0_init
             diff[1] += LinearAlgebra.norm(res[k] - v)
         end
+
         @test (diff[1] < 1e-3)
+
+        # Obtain small signal results for initial conditions
+        small_sig = small_signal_analysis(sim)
+        eigs = small_sig.eigenvalues
+        @test small_sig.stable
+
+        # Test Eigenvalues
         @test LinearAlgebra.norm(eigs - test14_eigvals) < 1e-3
-        @test res.solution.retcode == :Success
+
+        #Run simulation
+        @test execute!(
+            sim, #simulation structure
+            Rodas4(),#Sundials DAE Solver
+            dtmax = 0.001, #keywords arguments
+        ) == PSID.SIMULATION_FINALIZED
+        results = read_results(sim)
+
+        series = get_state_series(results, ("generator-101-1", :ω_oc))
     finally
         @info("removing test files")
         rm(path, force = true, recursive = true)
