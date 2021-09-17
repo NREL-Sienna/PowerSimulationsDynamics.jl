49d63abf08839cd85f696b021511bcf599f63536
Jose Daniel Lara <jdlara@berkeley.edu>
2021-09-16 23:12:36 -0700

    add new wrapper

--- src/base/simulation_results.jl
+++ src/base/simulation_results.jl
@@ -2,10 +2,7 @@ struct SimulationResults
     global_index::MAPPING_DICT
     voltage_buses::Vector{Int}
     current_buses::Vector{Int}
-<<<<<<< HEAD
     solution::SciMLBase.AbstractODESolution
-=======
->>>>>>> c0220169 (change wrapper name)
 end
 
 function make_global_state(wrapped_devices)
