d8b6abd1083e3c1de4e410e3898e6e97c30c0bd0
Rodrigo Henriquez <rodrigomha@gmail.com>
2021-09-17 16:27:53 -0700

    fix test 25

--- test/test_case25_multimach_dynlines.jl
+++ test/test_case25_multimach_dynlines.jl
@@ -17,14 +17,14 @@ include(joinpath(TEST_FILES_DIR, "data_tests/test25.jl"))
 ############### SOLVE PROBLEM ####################
 ##################################################
 
-#time span
+# time span
 tspan = (0.0, 40.0);
 
-#PSCAD benchmark data
+# PSCAD benchmark data
 csv_file = joinpath(TEST_FILES_DIR, "benchmarks/pscad/Test25/Test25_v102.csv")
 t_offset = 49.0
 
-#Define Fault using Callbacks
+# Define Fault using Callbacks
 gen2 = get_dynamic_injector(get_component(Generator, sys, "generator-102-1"));
 Pref_change = ControlReferenceChange(1.0, gen2, :P_ref, 0.9);
 
@@ -32,35 +32,37 @@ Pref_change = ControlReferenceChange(1.0, gen2, :P_ref, 0.9);
     path = (joinpath(pwd(), "test-25"))
     !isdir(path) && mkdir(path)
     try
-        #Define Simulation Problem
+        # Define Simulation Problem
         sim = Simulation!(ResidualModel, sys, path, tspan, Pref_change)
 
-        small_sig = small_signal_analysis(sim)
-        @test small_sig.stable
-
-        #Solve problem in equilibrium
-        @test execute!(sim, Sundials.IDA(), dtmax = 0.01, saveat = 0.01) ==
-              PSID.SIMULATION_FINALIZED
-        results = read_results(sim)
-
-        #Obtain voltage magnitude data
-        series = get_voltage_magnitude_series(results, 102)
-        t = series[1]
-        v = series[2]
-
-        #Obtain benchmark data from PSCAD
-        M = get_csv_data(csv_file)
-        t_pscad = M[:, 1] .- t_offset
-        v_pscad = M[:, 2]
-
+        # Test Initial Condition
         diff = [0.0]
         res = get_init_values_for_comparison(sim)
         for (k, v) in test25_x0_init
             diff[1] += LinearAlgebra.norm(res[k] - v)
         end
         @test (diff[1] < 1e-3)
-        @test res.solution.retcode == :Success
-        #relaxed constraint to account for mismatch in damping
+
+        # Obtain small signal results for initial conditions
+        small_sig = small_signal_analysis(sim)
+        @test small_sig.stable
+
+        # Solve problem in equilibrium
+        @test execute!(sim, Sundials.IDA(), dtmax = 0.01, saveat = 0.01) ==
+              PSID.SIMULATION_FINALIZED
+        results = read_results(sim)
+
+        # Obtain voltage magnitude data
+        series = get_voltage_magnitude_series(results, 102)
+        t = series[1]
+        v = series[2]
+
+        # Obtain benchmark data from PSCAD
+        M = get_csv_data(csv_file)
+        t_pscad = M[:, 1] .- t_offset
+        v_pscad = M[:, 2]
+
+        # Relaxed constraint to account for mismatch in damping
         @test LinearAlgebra.norm(v - v_pscad) <= 0.05
         @test LinearAlgebra.norm(t - round.(t_pscad, digits = 3)) == 0.0
     finally
@@ -73,35 +75,37 @@ end
     path = (joinpath(pwd(), "test-25"))
     !isdir(path) && mkdir(path)
     try
-        #Define Simulation Problem
+        # Define Simulation Problem
         sim = Simulation!(MassMatrixModel, sys, path, tspan, Pref_change)
 
-        small_sig = small_signal_analysis(sim)
-        @test small_sig.stable
-
-        #Solve problem in equilibrium
-        @test execute!(sim, Rodas5(autodiff = true), dtmax = 0.01, saveat = 0.01) ==
-              PSID.SIMULATION_FINALIZED
-        results = read_results(sim)
-
-        #Obtain voltage magnitude data
-        series = get_voltage_magnitude_series(results, 102)
-        t = series[1]
-        v = series[2]
-
-        #Obtain benchmark data from PSCAD
-        M = get_csv_data(csv_file)
-        t_pscad = M[:, 1] .- t_offset
-        v_pscad = M[:, 2]
-
+        # Test Initial Condition
         diff = [0.0]
         res = get_init_values_for_comparison(sim)
         for (k, v) in test25_x0_init
             diff[1] += LinearAlgebra.norm(res[k] - v)
         end
         @test (diff[1] < 1e-3)
-        @test res.solution.retcode == :Success
-        #relaxed constraint to account for mismatch in damping
+
+        # Obtain small signal results for initial conditions
+        small_sig = small_signal_analysis(sim)
+        @test small_sig.stable
+
+        # Solve problem in equilibrium
+        @test execute!(sim, Rodas4(), dtmax = 0.01, saveat = 0.01) ==
+              PSID.SIMULATION_FINALIZED
+        results = read_results(sim)
+
+        # Obtain voltage magnitude data
+        series = get_voltage_magnitude_series(results, 102)
+        t = series[1]
+        v = series[2]
+
+        # Obtain benchmark data from PSCAD
+        M = get_csv_data(csv_file)
+        t_pscad = M[:, 1] .- t_offset
+        v_pscad = M[:, 2]
+
+        # Relaxed constraint to account for mismatch in damping
         @test LinearAlgebra.norm(v - v_pscad) <= 0.05
         @test LinearAlgebra.norm(t - round.(t_pscad, digits = 3)) == 0.0
     finally
