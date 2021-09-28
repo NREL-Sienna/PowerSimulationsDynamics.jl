eef298a6486fe823f2f1cd0491e7278558127fe5
Rodrigo Henriquez <rodrigomha@gmail.com>
2021-09-28 13:34:15 -0700

    add new getter functions

--- src/base/simulation.jl
+++ src/base/simulation.jl
@@ -398,3 +398,11 @@ end
 function read_results(sim::Simulation)
     return sim.results
 end
+
+function get_dynamic_wrapper(sim::Simulation, name::AbstractString)
+    return get_dynamic_wrapper(get_simulation_inputs(sim), name)
+end
+
+function get_control_references(sim::Simulation)
+    return get_control_references(get_simulation_inputs(sim))
+end
