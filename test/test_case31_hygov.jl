493ddfd44299190cbcd0b1a432c89c5d931c9de5
Jose Daniel Lara <jdlara@berkeley.edu>
2021-10-06 15:59:01 -0700

    add mass matrix test

--- test/test_case31_hygov.jl
+++ test/test_case31_hygov.jl
@@ -49,7 +49,7 @@ csv_file = joinpath(TEST_FILES_DIR, "benchmarks/psse/HYGOV/HYGOV_RESULTS.csv")
               PSID.SIMULATION_FINALIZED
         results = read_results(sim)
 
-        # Obtain data for angles
+        # Obtain results
         t, δ = get_state_series(results, ("generator-102-1", :δ))
         _, Vt = get_voltage_magnitude_series(results, 102)
         _, ω = get_state_series(results, ("generator-102-1", :ω))
@@ -110,18 +110,24 @@ end
               PSID.SIMULATION_FINALIZED
         results = read_results(sim)
 
-        # Obtain data for angles
-        series = get_state_series(results, ("generator-102-1", :δ))
-        t = series[1]
-        δ = series[2]
+        # Obtain results
+        t, δ = get_state_series(results, ("generator-102-1", :δ))
+        _, Vt = get_voltage_magnitude_series(results, 102)
+        _, ω = get_state_series(results, ("generator-102-1", :ω))
 
         # Obtain PSSE results
-        #t_psse, δ_psse = get_csv_delta(csv_file)
+        M = get_csv_data(csv_file)
+        t_psse = M[:, 1]
+        Vt_psse = M[:, 2]
+        δ_psse = M[:, 3]
+        ω_psse = M[:, 4] .+ 1.0
 
         # Test Transient Simulation Results
         # PSSE results are in Degrees
-        #@test LinearAlgebra.norm(δ - (δ_psse .* pi / 180), Inf) <= 1e-2
-        #@test LinearAlgebra.norm(t - round.(t_psse, digits = 3)) == 0.0
+        @test LinearAlgebra.norm(δ - (δ_psse .* pi / 180), Inf) <= 1e-2
+        @test LinearAlgebra.norm(t - round.(t_psse, digits = 3)) == 0.0
+        @test LinearAlgebra.norm(Vt - Vt_psse, Inf) <= 1e-3
+        @test LinearAlgebra.norm(ω - ω_psse, Inf) <= 1e-3
     finally
         @info("removing test files")
         rm(path, force = true, recursive = true)
