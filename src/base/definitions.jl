02a132bc36dc0c5e411ece5600b2551c4a8079e5
Jose Daniel Lara <jdlara@berkeley.edu>
2021-10-14 16:11:31 -0400

    remove negation

--- src/base/definitions.jl
+++ src/base/definitions.jl
@@ -149,6 +149,6 @@ Defines the status of the simulation object
     CONVERTED_FOR_SMALL_SIGNAL = 8
 end
 
-const _PROG_METER_ENABLED = !isa(stderr, Base.TTY) || (get(ENV, "CI", nothing) == "true")
+const _PROG_METER_ENABLED = isa(stderr, Base.TTY) || (get(ENV, "CI", nothing) == "true")
 
 const BUILD_TIMER = TimerOutputs.TimerOutput()
