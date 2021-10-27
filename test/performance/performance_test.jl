8c2e8b438de24eb10f9a6e4d6f386e61697c31d0
Jose Daniel Lara <jdlara@berkeley.edu>
2021-10-27 16:06:20 -0700

    update performance test call

--- test/performance/performance_test.jl
+++ test/performance/performance_test.jl
@@ -21,16 +21,19 @@ sys = System(
 )
 
 # First runs
-
-for m in [ResidualModel, MassMatrixModel]
-    Simulation(
-        m,
-        sys,
-        pwd(),
-        (0.0, 20.0), #time span
-        BranchTrip(1.0, Line, "CORONADO    -1101-PALOVRDE    -1401-i_10");
-        console_level = Logging.Error,
-    )
+try
+    for m in [ResidualModel, MassMatrixModel]
+        Simulation(
+            m,
+            sys,
+            pwd(),
+            (0.0, 20.0), #time span
+            BranchTrip(1.0, Line, "CORONADO    -1101-PALOVRDE    -1401-i_10");
+            console_level = Logging.Error,
+        )
+    end
+catch e
+    @error exception = (e, catch_backtrace())
 end
 
 try
@@ -42,13 +45,21 @@ try
         BranchTrip(1.0, Line, "CORONADO    -1101-PALOVRDE    -1401-i_10");
         console_level = Logging.Error,
     )
-    open("execute_time.txt", "a") do io
-        write(io, "| $(ARGS[1])-Build ResidualModel | $(time_build_ida) |\n")
+    status = execute!(sim_ida, IDA(), dtmax = 0.01)
+    if status == PSID.SIMULATION_FINALIZED
+        res_ida = read_results(sim_ida)
+        solve_time = res_ida.time_log[:timed_solve_time]
+        open("execute_time.txt", "a") do io
+            write(io, "| $(ARGS[1])-Build ResidualModel | $(time_build_ida) |\n")
+            write(io, "| $(ARGS[1])-Execute ResidualModel | $(solve_time) |\n")
+        end
+    else
+        error("FAILED TO SOLVE")
     end
 catch e
     @error exception = (e, catch_backtrace())
     open("execute_time.txt", "a") do io
-        write(io, "| $(ARGS[1])-Build ResidualModel | FAILED TO BUILD |\n")
+        write(io, "| $(ARGS[1])- ResidualModel | FAILED TO TEST |\n")
     end
 end
 
@@ -61,36 +72,20 @@ try
         BranchTrip(1.0, Line, "CORONADO    -1101-PALOVRDE    -1401-i_10");
         console_level = Logging.Error,
     ) #Type of Fault
-    open("execute_time.txt", "a") do io
-        write(io, "| $(ARGS[1])-Build MassMatrixModel | $(time_build_rodas) |\n")
+    status = execute!(sim_rodas, Rodas4(), dtmax = 0.01)
+    if status == PSID.SIMULATION_FINALIZED
+        res_rodas = read_results(sim_ida)
+        solve_time = res_rodas.time_log[:timed_solve_time]
+        open("execute_time.txt", "a") do io
+            write(io, "| $(ARGS[1])-Build MassMatrixModel | $(time_build_rodas) |\n")
+            write(io, "| $(ARGS[1])-Execute MassMatrixModel | $(solve_time) |\n")
+        end
+    else
+        error("FAILED TO SOLVE")
     end
 catch e
     @error exception = (e, catch_backtrace())
     open("execute_time.txt", "a") do io
-        write(io, "| $(ARGS[1])-Build MassMatrixModel | FAILED TO BUILD |\n")
+        write(io, "| $(ARGS[1])-MassMatrixModel | FAILED TO TEST |\n")
     end
 end
-
-#=
-try
-    status = execute!(sim_rodas, Rodas4(), dtmax = 0.01)
-    res_rodas = read_results(sim_rodas)
-catch
-    time = "EXECUTE FAILED"
-finally
-    open("execute_time.txt", "a") do io
-    write(io, "| $(ARGS[1]) | 999 |\n")
-    end
-end
-
-try
-    status = execute!(sim_ida, IDA(), dtmax = 0.01)
-    res_ida = read_results(sim_ida)
-catch
-    time = "EXECUTE FAILED"
-finally
-    open("execute_time.txt", "a") do io
-    write(io, "| $(ARGS[1]) | 999 |\n")
-    end
-end
-=#
