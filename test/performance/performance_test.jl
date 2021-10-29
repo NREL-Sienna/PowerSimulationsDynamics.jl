546c386188a4eb15fa2720c83185d6f0e9cb8141
Jose Daniel Lara <jdlara@berkeley.edu>
2021-10-29 10:14:01 -0700

    add no_init to performance tests

--- test/performance/performance_test.jl
+++ test/performance/performance_test.jl
@@ -64,9 +64,9 @@ try
         pwd(),
         (0.0, 20.0), #time span
         BranchTrip(1.0, Line, "CORONADO    -1101-PALOVRDE    -1401-i_10");
-        console_level = Logging.Error,
+        #console_level = Logging.Error,
     ) #Type of Fault
-    status = execute!(sim_rodas, Rodas4(), dtmax = 0.01, enable_progress_bar = false)
+    status = execute!(sim_rodas, Rodas4(), abstol = 1e-9, initializealg  = NoInit())
     if status == PSID.SIMULATION_FINALIZED
         res_rodas = read_results(sim_rodas)
         solve_time = res_rodas.time_log[:timed_solve_time]
