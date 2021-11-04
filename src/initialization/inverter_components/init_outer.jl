cdfc6b6864a950370122bc0a657e4059a2f8c76f
Rodrigo Henriquez <rodrigomha@gmail.com>
2021-11-04 16:52:54 -0700

    add init for V_Flag = 0 (that fails for now)

--- src/initialization/inverter_components/init_outer.jl
+++ src/initialization/inverter_components/init_outer.jl
@@ -316,11 +316,11 @@ function initialize_outer!(
         X_c = PSY.get_R_c(reactive_power_control)
         VC_Flag = PSY.get_VC_Flag(reactive_power_control)
         V_reg = sqrt(V_R^2 + V_I^2)
-        #Compute input to the compensated voltage filter
+        # Compute input to the compensated voltage filter
         if VC_Flag == 0
             V_flt_input = V_reg + K_c * q_elec_out
         else
-            #V_flt_input = | V_reg - (R_c + jX_c)(I_r + jI_i) |
+            # Calculate compensated voltage: | V_reg - (R_c + jX_c)(I_r + jI_i) |
             V_flt_input = sqrt(
                 V_reg^2 +
                 2 * V_reg * (I_I * X_c - I_R * R_c) +
@@ -332,6 +332,36 @@ function initialize_outer!(
         internal_states[state_ct + 1] = q_elec_out / K_i
         internal_states[state_ct + 2] = q_elec_out
         internal_states[state_ct + 3] = V_t / K_qi
+        #Update Inner Vars
+        inner_vars[V_oc_var] = 0.0
+        inner_vars[Iq_oc_var] = q_elec_out / max(V_t, 0.01)
+    elseif Ref_Flag == 1 && PF_Flag == 0 && V_Flag == 0
+        # TODO: Fix and debug this case when Q_Flag = 1
+        K_i = PSY.get_K_i(reactive_power_control)
+        K_qi = PSY.get_K_qi(reactive_power_control)
+        K_c = PSY.get_K_c(reactive_power_control)
+        R_c = PSY.get_R_c(reactive_power_control)
+        X_c = PSY.get_R_c(reactive_power_control)
+        VC_Flag = PSY.get_VC_Flag(reactive_power_control)
+        V_reg = sqrt(V_R^2 + V_I^2)
+        # Compute input to the compensated voltage filter
+        if VC_Flag == 0
+            V_flt_input = V_reg + K_c * q_elec_out
+        else
+            # Calculate compensated voltage: | V_reg - (R_c + jX_c)(I_r + jI_i) |
+            V_flt_input = sqrt(
+                V_reg^2 +
+                2 * V_reg * (I_I * X_c - I_R * R_c) +
+                (I_I^2 + I_R^2) * (R_c^2 + X_c^2),
+            )
+        end
+        #Update states
+        internal_states[state_ct] = V_flt_input #Vc_flt
+        internal_states[state_ct + 1] = q_elec_out / K_i
+        internal_states[state_ct + 2] = q_elec_out
+        #Update Inner Vars
+        @show inner_vars[V_oc_var] = V_flt_input - V_t
+        inner_vars[Iq_oc_var] = V_flt_input / max(V_t, 0.01)
     else
         error("Flags for Generic Renewable Model not supported yet")
     end
