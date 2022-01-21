c1f74ad156e18327cbbf9c5df971edcb74c82cb3
Jose Daniel Lara <jdlara@berkeley.edu>
2022-01-20 20:59:58 -0600

    add and test jacobian methods

--- src/base/jacobian.jl
+++ src/base/jacobian.jl
@@ -11,57 +11,7 @@ struct JacobianFunctionWrapper{
     x::Vector{Float64}
 end
 
-function JacobianFunctionWrapper(
-    m!::SystemModel{MassMatrixModel},
-    x0_guess::Vector{Float64};
-    # Improve the heuristic to do sparsity detection
-    sparse_retrieve_loop = 3,
-)
-    x0 = deepcopy(x0_guess)
-    n = length(x0)
-    m_ = (residual, x) -> m!(residual, x, nothing, 0.0)
-    jconfig = ForwardDiff.JacobianConfig(m_, similar(x0), x0, ForwardDiff.Chunk(x0))
-    Jf = (Jv, x) -> begin
-        @debug "Evaluating Jacobian Function"
-        ForwardDiff.jacobian!(Jv, m_, zeros(n), x, jconfig)
-        return
-    end
-    jac = zeros(n, n)
-    for _ in 1:sparse_retrieve_loop
-        temp = zeros(n, n)
-        Jf(temp, abs.(x0 + Random.rand(n) - Random.rand(n)))
-        jac .+= abs.(temp)
-    end
-    Jv = SparseArrays.sparse(jac)
-    Jf(Jv, x0)
-    return JacobianFunctionWrapper{typeof(Jf), typeof(Jv)}(Jf, Jv, x0)
-end
-
-function JacobianFunctionWrapper(
-    m!::SystemModel{ResidualModel},
-    x0::Vector{Float64};
-    # Improve the heuristic to do sparsity detection
-    sparse_retrieve_loop = 3,
-)
-    n = length(x0)
-    m_ = (residual, x) -> m!(residual, zeros(n), x, nothing, 0.0)
-    jconfig = ForwardDiff.JacobianConfig(m_, similar(x0), x0, ForwardDiff.Chunk(x0))
-    Jf = (Jv, x) -> begin
-        @debug "Evaluating Jacobian Function"
-        ForwardDiff.jacobian!(Jv, m_, zeros(n), x, jconfig)
-        return
-    end
-    jac = zeros(n, n)
-    for _ in 1:sparse_retrieve_loop
-        temp = zeros(n, n)
-        Jf(temp, abs.(x0 + Random.rand(n) - Random.rand(n)))
-        jac .+= abs.(temp)
-    end
-    Jv = SparseArrays.sparse(jac)
-    Jf(Jv, x0)
-    return JacobianFunctionWrapper{typeof(Jf), typeof(Jv)}(Jf, Jv, x0)
-end
-
+# This version of the function is type unstable should only be used for non-critial
 function (J::JacobianFunctionWrapper)(x::AbstractVector{Float64})
     J.x .= x
     return J.Jf(J.Jv, x)
@@ -82,8 +32,7 @@ function (J::JacobianFunctionWrapper)(
     p,
     t,
 ) where {U <: Union{Matrix{Float64}, SparseArrays.SparseMatrixCSC{Float64, Int64}}}
-    J.x .= x
-    J.Jf(JM, x)
+    J(JM, x)
     return
 end
 
@@ -92,20 +41,88 @@ function (J::JacobianFunctionWrapper)(
     dx::AbstractVector{Float64},
     x::AbstractVector{Float64},
     p,
-    gamma,
+    gamma::Float64,
     t,
 ) where {U <: Union{Matrix{Float64}, SparseArrays.SparseMatrixCSC{Float64, Int64}}}
-    J.x .= x
-    JM .= gamma * LinearAlgebra.Diagonal(ones(length(x))) .- J.Jf(JM, x)
-    return JM
+    J(JM, x)
+    JM .= gamma * LinearAlgebra.Diagonal(ones(length(x))) .+ JM
+    return
+end
+
+function JacobianFunctionWrapper(
+    m!::SystemModel{MassMatrixModel},
+    x0_guess::Vector{Float64};
+    # Improve the heuristic to do sparsity detection
+    sparse_retrieve_loop::Int = 3,
+)
+    x0 = deepcopy(x0_guess)
+    n = length(x0)
+    m_ = (residual, x) -> m!(residual, x, nothing, 0.0)
+    jconfig = ForwardDiff.JacobianConfig(m_, similar(x0), x0, ForwardDiff.Chunk(x0))
+    Jf = (Jv, x) -> begin
+        @debug "Evaluating Jacobian Function"
+        ForwardDiff.jacobian!(Jv, m_, zeros(n), x, jconfig)
+        return
+    end
+    jac = zeros(n, n)
+    if sparse_retrieve_loop > 0
+        for _ in 1:sparse_retrieve_loop
+            temp = zeros(n, n)
+            Jf(temp, abs.(x0 + Random.rand(n) - Random.rand(n)))
+            jac .+= abs.(temp)
+        end
+        Jv = SparseArrays.sparse(jac)
+    elseif sparse_retrieve_loop == 0
+        Jv = jac
+    else
+        throw(IS.ConflictingInputsError("negative sparse_retrieve_loop not valid"))
+    end
+    Jf(Jv, x0)
+    return JacobianFunctionWrapper{typeof(Jf), typeof(Jv)}(Jf, Jv, x0)
+end
+
+function JacobianFunctionWrapper(
+    m!::SystemModel{ResidualModel},
+    x0::Vector{Float64};
+    # Improve the heuristic to do sparsity detection
+    sparse_retrieve_loop::Int = 3,
+)
+    n = length(x0)
+    m_ = (residual, x) -> m!(residual, zeros(n), x, nothing, 0.0)
+    jconfig = ForwardDiff.JacobianConfig(m_, similar(x0), x0, ForwardDiff.Chunk(x0))
+    Jf = (Jv, x) -> begin
+        @debug "Evaluating Jacobian Function"
+        ForwardDiff.jacobian!(Jv, m_, zeros(n), x, jconfig)
+        return
+    end
+    jac = zeros(n, n)
+    if sparse_retrieve_loop > 0
+        for _ in 1:sparse_retrieve_loop
+            temp = zeros(n, n)
+            Jf(temp, abs.(x0 + Random.rand(n) - Random.rand(n)))
+            jac .+= abs.(temp)
+        end
+        Jv = SparseArrays.sparse(jac)
+    elseif sparse_retrieve_loop == 0
+        Jv = jac
+    else
+        throw(IS.ConflictingInputsError("negative sparse_retrieve_loop not valid"))
+    end
+    Jf(Jv, x0)
+    return JacobianFunctionWrapper{typeof(Jf), typeof(Jv)}(Jf, Jv, x0)
 end
 
 function get_jacobian(
     ::Type{T},
     inputs::SimulationInputs,
     x0_init::Vector{Float64},
+    sparse_retrieve_loop::Int,
 ) where {T <: SimulationModel}
-    return JacobianFunctionWrapper(T(inputs, x0_init, JacobianCache), x0_init)
+    return JacobianFunctionWrapper(
+        T(inputs, x0_init, JacobianCache),
+        x0_init;
+        sparse_retrieve_loop = sparse_retrieve_loop,
+    )
 end
 
 function _set_operating_point!(
@@ -120,11 +137,29 @@ function _set_operating_point!(
     return status
 end
 
-function get_jacobian(T, system::PSY.System)
+"""
+    function get_jacobian(
+    ::Type{T},
+    system::PSY.System,
+    sparse_retrieve_loop::Int = 3,
+    ) where {T <: SimulationModel}
+
+Returns the jacobian function of the system model resulting from the system data.
+
+# Arguments:
+- `::SimulationModel` : Type of Simulation Model. `ResidualModel` or `MassMatrixModel`. See [Models Section](https://nrel-siip.github.io/PowerSimulationsDynamics.jl/stable/models/) for more details
+- `system::PowerSystems.System` : System data
+- `sparse_retrieve_loop::Int` : Number of loops for sparsity detection. If 0, builds the Jacobian with a DenseMatrix
+"""
+function get_jacobian(
+    ::Type{T},
+    system::PSY.System,
+    sparse_retrieve_loop::Int = 3,
+) where {T <: SimulationModel}
     # Deepcopy avoid system modifications
     simulation_system = deepcopy(system)
     inputs = SimulationInputs(T, simulation_system, ReferenceBus)
     x0_init = get_flat_start(inputs)
     _set_operating_point!(x0_init, inputs, system)
-    return get_jacobian(T, inputs, x0_init)
+    return get_jacobian(T, inputs, x0_init, sparse_retrieve_loop)
 end
