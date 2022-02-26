ff716723abce691eec3c4c84b6111fce671c9587
Jose Daniel Lara <jdlara@berkeley.edu>
2022-02-26 15:40:52 -0800

    Add files via upload (#191)

--- test/benchmarks/psse/python_scripts/main.py
+++ test/benchmarks/psse/python_scripts/main.py
@@ -1,38 +1,16 @@
 import PSSEInterface
-import os
+reload(PSSEInterface)
 
-powerflow_file='C:\Users\jlara\Desktop\case\mod_ren14.raw'         
-dynamic_data_file='C:\Users\jlara\Desktop\case\dyn_data_14bus_mod.dyr'         
+powerflow_file='WECC240_v04_DPV_RE20_v33_6302_xfmr_DPbuscode_PFadjusted_V32.raw'         
+dynamic_data_file='WECC240_dynamics_UPV_v04_all.dyr'         
 print '\nInput Files ...'
 print powerflow_file    
 print dynamic_data_file
 
-csv_file='results.csv'
-output = '.\\14-Bus'        
-print '\Output files...'
-print csv_file
+output_dir = '.\\240Bus'        
 
-PSSEInterface.init_system(powerflow_file)
-perturbations = PSSEInterface.get_line_trips()
-slack_bus = PSSEInterface.get_slack_bus()
+PSSEInterface.initialize_system(powerflow_file)
+all_perturbations = PSSEInterface.get_generator_trips()[0:2]
+#perturbation = ("Line", PSSEInterface.LineTrip(2407, 2408, r"""1"""))
 
-for p in perturbations:
-    PSSEInterface.FileSystemSetUp(output, p[0])
-    PSSEInterface.init_system(powerflow_file)
-    PSSEInterface.PowerFlowConvert()
-    PSSEInterface.SetUpDynamicSimulation(dynamic_data_file, convergence_tolerance = 0.0001, delta_t = 0.005)
-
-    signals = [
-            1, #ANGLE,
-            2, #PELEC,
-            3, #QELEC, 
-            4, #ETERM,
-            5, #EFD,
-            6, #PMECH,
-            7, #SPEED,
-            #8, #XADIFID, 
-        ]
-    slack_channel = PSSEInterface.SetUpOutputChannels(output, p[0], signals, slack_bus)
-    PSSEInterface.RunSimulation(p[1], tspan = (0.0, 10.0), fault_time = 1.0)
-    PSSEInterface.ProcessResults(output, p[0], csv_file, slack_channel)
-    PSSEInterface.CloseSession()
+PSSEInterface.run_perturbations(all_perturbations, output_dir, powerflow_file, dynamic_data_file, signals = [PSSEInterface.PELEC, PSSEInterface.QELEC])
