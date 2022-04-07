ca0ca1dfcd6a63097a50fd08917a856359eb3527
Jose Daniel Lara <jdlara@berkeley.edu>
2022-04-06 23:52:17 -0700

    use int

--- src/base/simulation.jl
+++ src/base/simulation.jl
@@ -279,7 +279,7 @@ end
 function _get_jacobian(sim::Simulation{T}) where {T <: SimulationModel}
     inputs = get_simulation_inputs(sim)
     x0_init = get_initial_conditions(sim)
-    return JacobianFunctionWrapper(T(inputs, x0_init, JacobianCache), x0_init; sparse_retrieve_loop = 0.0)
+    return JacobianFunctionWrapper(T(inputs, x0_init, JacobianCache), x0_init; sparse_retrieve_loop = 0)
 end
 
 function _build_perturbations!(sim::Simulation)
