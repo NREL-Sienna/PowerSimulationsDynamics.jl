f084da0226a930fc182c74ffb5859859d8161f8c
Rodrigo Henriquez <rodrigomha@gmail.com>
2022-05-17 11:40:55 -0700

    add test 37 for induction motor

--- test/test_case37_InductionMotor.jl
+++ test/test_case37_InductionMotor.jl
@@ -21,41 +21,30 @@ dyr_file = joinpath(TEST_FILES_DIR, "data_tests/TVC_System_motor.dyr")
     !isdir(path) && mkdir(path)
     try
         sys = System(raw_file, dyr_file)
-        # correct X_thevenin
-        source = first(get_components(Source, sys))
-        PSY.set_X_th!(source, 0.01)
-        # Motor parameters 
- 
-        Ind_Motor() = PSY.SingleCageInductionMachine(
-            name="motor",
-            Rs=0.013,
-            Rr=0.009,
-            Xs=0.067,
-            Xr=0.17,
-            Xm=3.8,
-            H=1.5,
-            A=1.0,
-            B=0.0,
-            base_power=1000
-        )
-
-        # Include the induction motor
-        load = first(get_components(PSY.PowerLoad, sys))
-        dynamic_injector = Ind_Motor()
-        set_dynamic_injector!(load, dynamic_injector)
-
         time_span = (0.0, 20.0)
         perturbation_trip = BranchTrip(1.0, Line, "BUS1-BUS4-i_1")
+        # Create Simulation with Constant Power
+        sim_P = Simulation(ResidualModel, sys, path, time_span, perturbation_trip)
 
-        sim = Simulation!(ResidualModel, sys, pwd(), time_span, perturbation_trip)
+        # Motor parameters 
+        load = first(get_components(PSY.PowerLoad, sys))
+        # Include the induction motor
+        dynamic_injector = Ind_Motor(load)
+        set_dynamic_injector!(load, dynamic_injector)
+        sim = Simulation(ResidualModel, sys, path, time_span, perturbation_trip)
+
+        # Test initial voltages between two systems are equivalent
+        voltages_P = sim_P.x0_init[1:8]
+        voltages_motor = sim.x0_init[1:8]
+        @test LinearAlgebra.norm(voltages_P - voltages_motor) < 1e-4
 
         # Test Initial Condition
         diff = [0.0]
         res = get_init_values_for_comparison(sim)
-        for (k, v) in init_cond
+        for (k, v) in test37_x0_init
             diff[1] += LinearAlgebra.norm(res[k] - v)
         end
- 
+
         @test (diff[1] < 1e-3)
 
         # Obtain small signal results for initial conditions
@@ -64,19 +53,65 @@ dyr_file = joinpath(TEST_FILES_DIR, "data_tests/TVC_System_motor.dyr")
         @test small_sig.stable
 
         #Test Eigenvalues
-        @test LinearAlgebra.norm(eigs - eigs_value) < 1e-3
+        @test LinearAlgebra.norm(eigs - test37_eigvals) < 1e-3
 
         # Solve problem
         @test execute!(sim, IDA(), dtmax = 0.005, saveat = 0.005) ==
               PSID.SIMULATION_FINALIZED
         results = read_results(sim)
 
-        # Obtain data for bus voltages
-        V_1 = get_voltage_magnitude_series(results,1)
-        V_2 = get_voltage_magnitude_series(results,2)
-        V_3 = get_voltage_magnitude_series(results,3)
-        V_4 = get_voltage_magnitude_series(results,4)
+        # No comparison since model is not available in other tools
+    finally
+        @info("removing test files")
+        rm(path, force = true, recursive = true)
+    end
+end
 
+@testset "Test 5th_order Ind. Motor" begin
+    path = (joinpath(pwd(), "test-ind-mot_5th"))
+    !isdir(path) && mkdir(path)
+    try
+        sys = System(raw_file, dyr_file)
+        time_span = (0.0, 20.0)
+        perturbation_trip = BranchTrip(1.0, Line, "BUS1-BUS4-i_1")
+        # Create Simulation with Constant Power
+        sim_P = Simulation(MassMatrixModel, sys, path, time_span, perturbation_trip)
+
+        # Motor parameters 
+        load = first(get_components(PSY.PowerLoad, sys))
+        # Include the induction motor
+        dynamic_injector = Ind_Motor(load)
+        set_dynamic_injector!(load, dynamic_injector)
+        sim = Simulation(MassMatrixModel, sys, path, time_span, perturbation_trip)
+
+        # Test initial voltages between two systems are equivalent
+        voltages_P = sim_P.x0_init[1:8]
+        voltages_motor = sim.x0_init[1:8]
+        @test LinearAlgebra.norm(voltages_P - voltages_motor) < 1e-4
+
+        # Test Initial Condition
+        diff = [0.0]
+        res = get_init_values_for_comparison(sim)
+        for (k, v) in test37_x0_init
+            diff[1] += LinearAlgebra.norm(res[k] - v)
+        end
+
+        @test (diff[1] < 1e-3)
+
+        # Obtain small signal results for initial conditions
+        small_sig = small_signal_analysis(sim)
+        eigs = small_sig.eigenvalues
+        @test small_sig.stable
+
+        #Test Eigenvalues
+        @test LinearAlgebra.norm(eigs - test37_eigvals) < 1e-3
+
+        # Solve problem
+        @test execute!(sim, Rodas5(), dtmax = 0.005, saveat = 0.005) ==
+              PSID.SIMULATION_FINALIZED
+        results = read_results(sim)
+
+        # No comparison since model is not available in other tools
     finally
         @info("removing test files")
         rm(path, force = true, recursive = true)
