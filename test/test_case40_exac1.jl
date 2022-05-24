c2463fbf7c3ef215d8b97ad69befcc8645c5e87e
Rodrigo Henriquez <rodrigomha@gmail.com>
2022-05-24 15:25:17 -0700

    add massmatrix test 40

--- test/test_case40_exac1.jl
+++ test/test_case40_exac1.jl
@@ -80,3 +80,67 @@ csv_file = joinpath(TEST_FILES_DIR, "benchmarks/psse//EXAC1/results_PSSe.csv")
         rm(path, force = true, recursive = true)
     end
 end
+
+@testset "Test 40 EXAC1 MassMatrixModel" begin
+    path = joinpath(pwd(), "test-psse-exac1")
+    !isdir(path) && mkdir(path)
+    try
+        # Define Simulation Problem
+        sim = Simulation(
+            MassMatrixModel,
+            sys, #system
+            path,
+            (0.0, 20.0), #time span
+            BranchTrip(1.0, Line, "BUS1-BUS4-i_1"), #Type of Fault
+        )
+
+        # Test Initial Condition
+        diff_val = [0.0]
+        res = get_init_values_for_comparison(sim)
+        for (k, v) in test40_x0_init
+            diff_val[1] += LinearAlgebra.norm(res[k] - v)
+        end
+
+        @test diff_val[1] < 1e-3
+
+        # Obtain small signal results for initial conditions
+        small_sig = small_signal_analysis(sim)
+        eigs = small_sig.eigenvalues
+        @test small_sig.stable
+
+        # Test Eigenvalues
+        @test LinearAlgebra.norm(eigs - test40_eigvals) < 1e-3
+
+        # Solve problem
+        @test execute!(sim, Rodas4(), dtmax = 0.005, saveat = 0.005) ==
+              PSID.SIMULATION_FINALIZED
+        results = read_results(sim)
+
+        # Obtain results
+
+        t_psid, v2_psid = get_voltage_magnitude_series(results, 2)
+        _, v3_psid = get_voltage_magnitude_series(results, 3)
+        _, ω_psid = get_state_series(results, ("generator-2-1", :ω))
+
+        # Obtain PSSE results
+        M = get_csv_data(csv_file)
+        t_psse = M[:, 1]
+        v1_psse = M[:, 2]
+        v2_psse = M[:, 3]
+        v3_psse = M[:, 4]
+        v4_psse = M[:, 5]
+        ω_psse = M[:, 6] .+ 1.0
+        ve_psse = M[:, 7]
+
+        # Test Transient Simulation Results
+
+        @test LinearAlgebra.norm(t_psid - round.(t_psse, digits = 3)) == 0.0
+        @test LinearAlgebra.norm(v2_psid - v2_psse, Inf) <= 1e-3
+        @test LinearAlgebra.norm(v3_psid - v3_psse, Inf) <= 1e-3
+        @test LinearAlgebra.norm(ω_psid - ω_psse, Inf) <= 1e-3
+
+    finally
+        @info("removing test files")
+        rm(path, force = true, recursive = true)
+    end
+end
