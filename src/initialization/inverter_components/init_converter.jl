8fc50b696348e57cbf4cd85f20d13ded25403634
Rodrigo Henriquez <rodrigomha@gmail.com>
2022-06-07 19:05:54 -0700

    update init routines

--- src/initialization/inverter_components/init_converter.jl
+++ src/initialization/inverter_components/init_converter.jl
@@ -66,3 +66,55 @@ function initialize_converter!(
     inner_vars[Iq_ic_var] = -Iq
     return
 end
+
+
+function initialize_converter!(
+    device_states,
+    static::PSY.StaticInjection,
+    dynamic_device::DynamicWrapper{
+        PSY.DynamicInverter{PSY.RenewableEnergyVoltageConverterTypeA, O, IC, DC, P, F},
+    },
+    inner_vars::AbstractVector,
+) where {
+    O <: PSY.OuterControl,
+    IC <: PSY.InnerControl,
+    DC <: PSY.DCSource,
+    P <: PSY.FrequencyEstimator,
+    F <: PSY.Filter,
+}
+    #Get inner vars
+    Vr_filter = inner_vars[Vr_filter_var]
+    Vi_filter = inner_vars[Vi_filter_var]
+    θ = atan(Vi_filter, Vr_filter)
+    V_t = sqrt(Vr_filter^2 + Vi_filter^2)
+    Iq_external = inner_vars[Ii_filter_var]
+    Ip_external = inner_vars[Ir_filter_var]
+    #Reference Transformation
+    Ip = Ip_external * cos(-θ) - Iq_external * sin(-θ)
+    Iq = Ip_external * sin(-θ) + Iq_external * cos(-θ)
+    converter = PSY.get_converter(dynamic_device)
+    Io_lim = PSY.get_Io_lim(converter)
+    Vo_lim = PSY.get_Vo_lim(converter)
+
+    # Lv_pnt0 is unused in the initialization
+    _, Lv_pnt1 = PSY.get_Lv_pnts(converter)
+
+    if (Iq < Io_lim) || (V_t > Vo_lim) || (V_t < Lv_pnt1)
+        @error(
+            "Power flow solution outside of inverter limits $(PSY.get_name(static)). Update parameters."
+        )
+    end
+
+    #Update converter states
+    converter_ix = get_local_state_ix(dynamic_device, PSY.RenewableEnergyVoltageConverterTypeA)
+    converter_states = @view device_states[converter_ix]
+    converter_states[1] = Ip #Ip_cnv
+    converter_states[2] = -Iq #Iq_cnv
+    converter_states[3] = V_t #Vmeas
+
+    #Update entry to converter currents (inner currents)
+    inner_vars[Id_ic_var] = Ip
+    # Multiplied by -1 because PSS/e doesn't do a conjugate.
+    inner_vars[Iq_ic_var] = -Iq
+    return
+end
