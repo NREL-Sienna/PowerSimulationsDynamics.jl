4371d807adb087f427577b975a068e55e3002a52
Rodrigo Henríquez-Auba <rodrigomha@gmail.com>
2022-08-08 11:25:15 -0700

    Add series branch flow (#264)
    * add branch series functions
    
    * add exports
    
    * add TODO test
    
    * formatting

--- test/test_case26_SEXS.jl
+++ test/test_case26_SEXS.jl
@@ -77,8 +77,12 @@ function test_sexs_implicit(dyr_file, csv_file, init_cond, eigs_value)
         t = series[1]
         δ = series[2]
         V = series2[2]
-        # TODO: Test Vf with PSSE
+        # TODO: Test Vf with PSSE and branch series flows
         series4 = get_field_voltage_series(results, "generator-102-1")
+        series5 = get_activepower_branch_flow(results, "BUS 1-BUS 3-i_1", :from)
+        series6 = get_reactivepower_branch_flow(results, "BUS 1-BUS 3-i_1", :to)
+        series7 = get_real_current_branch_flow(results, "BUS 1-BUS 3-i_1")
+        series8 = get_imaginary_current_branch_flow(results, "BUS 1-BUS 3-i_1")
         Vf = series4[2]
 
         # TODO: Get PSSE CSV files and enable tests
