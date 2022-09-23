28ab366c55ca0fb44dbe16121783262499636ea3
m-bossart <bossart.matt@gmail.com>
2022-09-23 11:12:57 -0400

    use ARGS in check_folder

--- src/base/file_system.jl
+++ src/base/file_system.jl
@@ -1,8 +1,11 @@
 function check_folder(folder::String)
     !isdir(folder) && throw(IS.ConflictingInputsError("Specified folder is not valid"))
     try
-        mkdir(joinpath(folder, "fake"))
-        rm(joinpath(folder, "fake"))
+        fake_dir =
+            length(ARGS) > 0 ? joinpath(folder, string("fake_", ARGS[1])) :
+            joinpath(folder, "fake")
+        mkdir(fake_dir)
+        rm(fake_dir)
     catch e
         throw(IS.ConflictingInputsError("Specified folder does not have write access [$e]"))
     end
