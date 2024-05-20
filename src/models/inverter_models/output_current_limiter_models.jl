441febf2c07446976efb3e4429300a5a14f4ea02
HaleyRoss <haley.a.ross@gmail.com>
2024-05-19 19:35:33 -0600

    Add hybrid limiter control logic

--- src/models/inverter_models/output_current_limiter_models.jl
+++ src/models/inverter_models/output_current_limiter_models.jl
@@ -34,7 +34,24 @@ function limit_output_current(limiter :: PSY.SaturationOutputCurrentLimiter, Id_
         Id_cnv_ref2 = Id_cnv_ref
         Iq_cnv_ref2 = Iq_cnv_ref
     end
-    Del_Vv_d = gain*(Id_cnv_ref - Id_cnv_ref2)
-    Del_Vv_q = gain*(Iq_cnv_ref - Iq_cnv_ref2)
+    Del_Vv_d = gain * (Id_cnv_ref - Id_cnv_ref2)
+    Del_Vv_q = gain * (Iq_cnv_ref - Iq_cnv_ref2)
+    return Id_cnv_ref2, Iq_cnv_ref2, Del_Vv_d, Del_Vv_q
+end
+
+function limit_output_current(limiter :: PSY.HybridOutputCurrentLimiter, Id_cnv_ref :: Union{Float64, ForwardDiff.Dual}, Iq_cnv_ref :: Union{Float64, ForwardDiff.Dual}, ω :: Union{Float64, ForwardDiff.Dual})
+    limit_value = PSY.get_I_max(limiter)
+    real_imped = PSY.get_rv(limiter)
+    imag_imped = PSY.get_lv(limiter)
+    theta = atan(Iq_cnv_ref, Id_cnv_ref)
+    if (Id_cnv_ref^2 + Iq_cnv_ref^2)^(1/2) > limit_value 
+        Id_cnv_ref2 = limit_value*cos(theta)
+        Iq_cnv_ref2 = limit_value*sin(theta)
+    else
+        Id_cnv_ref2 = Id_cnv_ref
+        Iq_cnv_ref2 = Iq_cnv_ref
+    end
+    Del_Vv_d = ω * imag_imped * (Iq_cnv_ref - Iq_cnv_ref2) + real_imped * (Id_cnv_ref - Id_cnv_ref2)
+    Del_Vv_q = ω * imag_imped * (Id_cnv_ref - Id_cnv_ref2) + real_imped * (Iq_cnv_ref - Iq_cnv_ref2)
     return Id_cnv_ref2, Iq_cnv_ref2, Del_Vv_d, Del_Vv_q
 end
