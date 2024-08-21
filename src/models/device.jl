b5f4f39e08d122a76bf1bba5f197bbc14bfd8cd8
rodrigomha <rodrigomha@gmail.com>
2024-08-21 16:39:28 -0700

    update inner vars to update electric torque

--- src/models/device.jl
+++ src/models/device.jl
@@ -333,10 +333,14 @@ function _update_inner_vars!(
     I_d =
         (1.0 / (R^2 + Xq_pp * Xd_pp)) *
         (-R * (V_dq[1] - ψq_pp) + Xq_pp * (-V_dq[2] + ψd_pp))
+    I_q =
+        (1.0 / (R^2 + Xq_pp * Xd_pp)) * (Xd_pp * (V_dq[1] - ψq_pp) + R * (-V_dq[2] + ψd_pp))
     Se = saturation_function(machine, ψ_pp)
     Xad_Ifd = eq_p + (Xd - Xd_p) * (γ_d1 * I_d - γ_d2 * ψ_kd + γ_d2 * eq_p) + Se * ψd_pp
+    τ_e = I_d * (V_dq[1] + I_d * R) + I_q * (V_dq[2] + I_q * R)
 
     #Update Xad_Ifd
+    inner_vars[τe_var] = τ_e
     inner_vars[Xad_Ifd_var] = Xad_Ifd
     return
 end
@@ -386,11 +390,14 @@ function _update_inner_vars!(
 
     #Currents
     I_d = (1.0 / (R^2 + Xd_pp^2)) * (-R * (V_d + ψq_pp) + Xd_pp * (ψd_pp - V_q))
+    I_q = (1.0 / (R^2 + Xd_pp^2)) * (Xd_pp * (V_d + ψq_pp) + R * (ψd_pp - V_q))
     Se = saturation_function(machine, eq_p)
     Xad_Ifd =
         eq_p + Se * eq_p + (Xd - Xd_p) * (I_d + γ_d2 * (eq_p - ψ_kd - (Xd_p - Xl) * I_d))
+    τ_e = I_d * (V_d + I_d * R) + I_q * (V_q + I_q * R)
 
     #Update Xad_Ifd
+    inner_vars[τe_var] = τ_e
     inner_vars[Xad_Ifd_var] = Xad_Ifd
     return
 end
@@ -442,11 +449,14 @@ function _update_inner_vars!(
 
     #Currents
     I_d = (1.0 / (R^2 + Xd_pp^2)) * (-R * (V_d - ψq_pp) + Xq_pp * (-V_q + ψd_pp))
+    I_q = (1.0 / (R^2 + Xd_pp^2)) * (Xd_pp * (V_d - ψq_pp) + R * (-V_q + ψd_pp))
     Se = saturation_function(machine, ψ_pp)
     Xad_Ifd =
         eq_p + Se * ψd_pp + (Xd - Xd_p) * (I_d + γ_d2 * (eq_p - ψ_kd - (Xd_p - Xl) * I_d))
+    τ_e = I_d * (V_d + I_d * R) + I_q * (V_q + I_q * R)
 
     #Update Xad_Ifd
+    inner_vars[τe_var] = τ_e
     inner_vars[Xad_Ifd_var] = Xad_Ifd
     return
 end
