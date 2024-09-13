c4448810c497e617be7d3a819008c6b9fb8e9232
rodrigomha <rodrigomha@gmail.com>
2024-09-13 13:20:37 -0700

    remove omega term from clamp

--- src/models/inverter_models/output_current_limiter_models.jl
+++ src/models/inverter_models/output_current_limiter_models.jl
@@ -59,24 +59,40 @@ function limit_output_current(
     limiter::PSY.HybridOutputCurrentLimiter,
     Id_cnv_ref::ACCEPTED_REAL_TYPES,
     Iq_cnv_ref::ACCEPTED_REAL_TYPES,
-    ω::ACCEPTED_REAL_TYPES,
+    ::ACCEPTED_REAL_TYPES,
 )
     limit_value = PSY.get_I_max(limiter)
     real_imped = PSY.get_rv(limiter)
     imag_imped = PSY.get_lv(limiter)
     theta = atan(Iq_cnv_ref, Id_cnv_ref)
-    if (Id_cnv_ref^2 + Iq_cnv_ref^2)^(1 / 2) > limit_value
-        Id_cnv_ref2 = limit_value * cos(theta)
-        Iq_cnv_ref2 = limit_value * sin(theta)
+    use_clamp = get(PSY.get_ext(limiter), "nondifferentiable", true)
+    if use_clamp
+        if (Id_cnv_ref^2 + Iq_cnv_ref^2)^(1 / 2) > limit_value
+            Id_cnv_ref2 = limit_value * cos(theta)
+            Iq_cnv_ref2 = limit_value * sin(theta)
+        else
+            Id_cnv_ref2 = Id_cnv_ref
+            Iq_cnv_ref2 = Iq_cnv_ref
+        end
+        Del_Vv_d =
+            imag_imped * (Iq_cnv_ref2 - Iq_cnv_ref) +
+            real_imped * (Id_cnv_ref - Id_cnv_ref2)
+        Del_Vv_q =
+            imag_imped * (Id_cnv_ref - Id_cnv_ref2) +
+            real_imped * (Iq_cnv_ref - Iq_cnv_ref2)
+        return Id_cnv_ref2, Iq_cnv_ref2, Del_Vv_d, Del_Vv_q
     else
-        Id_cnv_ref2 = Id_cnv_ref
-        Iq_cnv_ref2 = Iq_cnv_ref
+        ε = 0.01
+        Idq = sqrt(Id_cnv_ref^2 + Iq_cnv_ref^2)
+        ρ = -ε*log(exp(-1/ε)+exp(-limit_value/(ε*Idq)))
+        Id_cnv_ref2 = ρ * Id_cnv_ref
+        Iq_cnv_ref2 = ρ * Iq_cnv_ref
+        Del_Vv_d =
+            imag_imped * (Iq_cnv_ref2 - Iq_cnv_ref) +
+            real_imped * (Id_cnv_ref - Id_cnv_ref2)
+        Del_Vv_q =
+            imag_imped * (Id_cnv_ref - Id_cnv_ref2) +
+            real_imped * (Iq_cnv_ref - Iq_cnv_ref2)
+        return Id_cnv_ref2, Iq_cnv_ref2, Del_Vv_d, Del_Vv_q
     end
-    Del_Vv_d =
-        ω * imag_imped * (Iq_cnv_ref2 - Iq_cnv_ref) +
-        real_imped * (Id_cnv_ref - Id_cnv_ref2)
-    Del_Vv_q =
-        ω * imag_imped * (Id_cnv_ref - Id_cnv_ref2) +
-        real_imped * (Iq_cnv_ref - Iq_cnv_ref2)
-    return Id_cnv_ref2, Iq_cnv_ref2, Del_Vv_d, Del_Vv_q
 end
